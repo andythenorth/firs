f9068a87982995f73636047b877dfeb6e89d9632
Terkhen <terkhen@openttd.org>
2010-09-29 15:24:47 +0200

    Change: clarify the code of the language script.

--- scripts/check_language.sh
+++ scripts/check_language.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-file=""
+translation_file=""
 
 cd `dirname $0`
 
 #Try to find a language that matches the parameter (02, spa, any, etc)
 if [ -n "$1" ]
 then
-	file="`find ../sprites/nfo/lang/ -iname "*$1*.pnfo"`"
+	translation_file="`find ../sprites/nfo/lang/ -iname "*$1*.pnfo"`"
 fi
 
 #If not found, use remove_defines.pnfo
-if [ -z "$file" ]
+if [ -z "$translation_file" ]
 then
-	file="../sprites/nfo/lang/remove_defines.pnfo"
+	translation_file="../sprites/nfo/lang/remove_defines.pnfo"
 fi
 
-echo "Missing strings at file $file"
-for text in `grep "#define" ../sprites/nfo/lang/7F_any.pnfo | cut -f2 -d " "`; do
-	value=`grep "$text" $file`
-	if [ -z "$value" ]
+echo "Missing strings at file $translation_file"
+for string_id in `grep -v "^//" ../sprites/nfo/lang/7F_any.pnfo | cut -f2 -s -d " "`; do
+	translated_string_id=`grep "$string_id" $translation_file`
+	if [ -z "$translated_string_id" ]
 	then
-		echo "$text"
+		echo "$string_id"
 	fi
 done
 
