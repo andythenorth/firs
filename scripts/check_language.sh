15f3be5d62d8af5d104729bbb396bea98d3e025d
Terkhen <terkhen@openttd.org>
2010-09-29 15:40:32 +0200

    Feature: the language script lists strings that have been changed since they were translated. Since this test only checks revisions, it can list as modified a string that has only changed its place in the file.

--- scripts/check_language.sh
+++ scripts/check_language.sh
@@ -25,6 +25,31 @@ for string_id in `grep -v "^//" ../sprites/nfo/lang/7F_any.pnfo | cut -f2 -s -d 
 	fi
 done
 
+#Use \n as field separator.
+old_IFS=$IFS
+IFS=$'\n'
+
+echo "Strings modified since last update at file $translation_file"
+`hg blame $translation_file | grep -v "^//" > /tmp/lang_blame.txt`
+for original_line in `hg blame ../sprites/nfo/lang/7F_any.pnfo | grep -v "//" | sed 's/^ *//'`; do
+	original_string_id=`echo $original_line | cut -f3 -s -d " "`
+	if [ -n "$original_string_id" ]
+	then
+		translated_line=`grep " $original_string_id " /tmp/lang_blame.txt | sed 's/^ //'`
+		if [ -n "$translated_line" ]
+		then
+			original_revision=`echo $original_line | cut -f1 -s -d ":"`
+			translated_revision=`echo $translated_line | cut -f1 -s -d ":"`
+			if [ "$original_revision" -gt "$translated_revision" ]
+			then
+				echo "$original_string_id (Original: r$original_revision, Translation: r$translated_revision)"
+			fi
+		fi
+	fi
+done
+#Restore the field separator.
+IFS=$old_IFS
+
 echo "Not required strings at file $translation_file"
 for string_id in `grep -v "^//" $translation_file | cut -f2 -s -d " "`; do
 	translated_string_id=`grep "$string_id" ../sprites/nfo/lang/7F_any.pnfo`
