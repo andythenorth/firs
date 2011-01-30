be89a353381f5dcbacd700d1323573c654ce4553
planetmaker <planetmaker@openttd.org>
2011-01-30 15:10:28 +0100

    Add: Ignore additional defines containing 'GENDER_' in the language files when checking their consistency

--- scripts/check_language.sh
+++ scripts/check_language.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 #Try to find a language that matches the parameter (02, spa, any, etc)
 if [ -n "$1" ]
 then
-	translation_file="`find ../sprites/nfo/lang/ -iname "*$1*.pnfo"`"
+	translation_file="`find ../sprites/nfo/lang/ -iname "*$1*.pnfo" | grep -v '.new.'`"
 fi
 
 #If not found, use remove_defines.pnfo
@@ -16,7 +16,7 @@ then
 	translation_file="../sprites/nfo/lang/remove_defines.pnfo"
 fi
 
-echo "Missing strings at file $translation_file"
+echo "Missing strings at file $translation_file:"
 for string_id in `grep -v "^//" ../sprites/nfo/lang/7F_any.pnfo | cut -f2 -s -d " "`; do
 	translated_string_id=`grep "$string_id" $translation_file`
 	if [ -z "$translated_string_id" ]
@@ -29,7 +29,7 @@ done
 old_IFS=$IFS
 IFS=$'\n'
 
-echo "Strings modified since last update at file $translation_file"
+echo "Strings modified since last update at file $translation_file:"
 `hg blame $translation_file | grep -v "^//" > /tmp/lang_blame.txt`
 for original_line in `hg blame ../sprites/nfo/lang/7F_any.pnfo | grep -v "//" | sed 's/^ *//'`; do
 	original_string_id=`echo $original_line | cut -f3 -s -d " "`
@@ -50,8 +50,8 @@ done
 #Restore the field separator.
 IFS=$old_IFS
 
-echo "Not required strings at file $translation_file"
-for string_id in `grep -v "^//" $translation_file | cut -f2 -s -d " "`; do
+echo "Not required strings at file $translation_file:"
+for string_id in `grep -v "^//" $translation_file | cut -f2 -s -d " " | grep -v "GENDER_"`; do
 	translated_string_id=`grep "$string_id" ../sprites/nfo/lang/7F_any.pnfo`
 	if [ -z "$translated_string_id" ]
 	then
