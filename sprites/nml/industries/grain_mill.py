7ca0d317e8d856482919b31f90e8141c7b360d33
Andy <andy@teamrubber.com>
2012-10-13 21:04:38 +0100

    Codechange: python template graphics filenames for Grain Mill

--- sprites/nml/industries/grain_mill.py
+++ sprites/nml/industries/grain_mill.py
@@ -3,17 +3,25 @@ import codecs # used for writing files - more unicode friendly than standard ope
 import os.path
 currentdir = os.curdir
 
-industry_name = 'grain_mill'
-
 from chameleon import PageTemplateLoader # chameleon used in most template cases
 # setup the places we look for templates
 templates = PageTemplateLoader(os.path.join(currentdir,'sprites','nml','templates'))
 industry_templates = PageTemplateLoader(os.path.join(currentdir,'sprites','nml','industries'))
 
+class Industry(object):
+    """Base class for all types of industry"""
+    def __init__(self, name):
+        self.name = name
+        self.graphics_file = '"sprites/graphics/industries/' + name + '.png"' # don't use os.path.join here, this is for nml
+        self.graphics_file_snow = '"sprites/graphics/industries/' + name + '_snow.png"' # don't use os.path.join here, this is for nml
+
+industry_name = 'grain_mill'
+industry = Industry(name=industry_name)
+
 # compile a single final nml file for the grf
 industry_template = industry_templates[industry_name + '.pypnml']
 
-templated_pnml = industry_template(industry_name = industry_name)
+templated_pnml = industry_template(industry = industry)
 
 pnml = codecs.open(os.path.join(currentdir,'sprites','nml','industries','grain_mill.pnml'), 'w','utf8')
 pnml.write(templated_pnml)
