4348ca982abeceeaf2dd9d917ed5cfc53a732bf2
Andy <andy@teamrubber.com>
2012-10-13 20:06:04 +0100

    Codechange: template grain mill pnml using Chameleon as test

--- sprites/nml/build.py
+++ sprites/nml/build.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
 print "[PYTHON] python preprocessing"
+
+import os.path
+currentdir = os.curdir
+
+import sys
+sys.path.append(os.path.join('industries')) # add to the module search path
+
+from industries import grain_mill
