68cdf1dad21194ab4661c0cf52e17b4381727542
Andy <andy@teamrubber.com>
2012-10-13 21:54:12 +0100

    Codechange: build some Grain Mill layouts using python templating

--- sprites/nml/industries/grain_mill.py
+++ sprites/nml/industries/grain_mill.py
@@ -10,13 +10,50 @@ industry_templates = PageTemplateLoader(os.path.join(currentdir,'sprites','nml',
 
 class Industry(object):
     """Base class for all types of industry"""
-    def __init__(self, name):
+    def __init__(self, name, layouts):
         self.name = name
         self.graphics_file = '"sprites/graphics/industries/' + name + '.png"' # don't use os.path.join here, this is for nml
         self.graphics_file_snow = '"sprites/graphics/industries/' + name + '_snow.png"' # don't use os.path.join here, this is for nml
+        self.layouts = layouts
+
+class Layout(object):
+    """Base class to hold industry layouts"""
+    def __init__(self, id, default_tile, tiles):
+        self.id = id
+        self.default_tile = default_tile
+        self.tiles = tiles
+
+    def render(self, industry):
+        template = templates['layout.pynml']
+        return template(layout=self, industry=industry)
+
+layout_1 = Layout('layout_1', default_tile = 'brickbakery_tile_3', tiles = (
+    (0, 0, 'brickbakery_tile_4'),
+    (1, 0, 'brickbakery_tile_1'),
+    (1, 1, 'brickbakery_tile_2'),
+))
+layout_2 = Layout('layout_2', default_tile = 'brickbakery_tile_3', tiles = (
+    (0, 0, 'brickbakery_tile_3'),
+    (0, 1, 'brickbakery_tile_4'),
+    (1, 0, 'brickbakery_tile_3'),
+    (1, 1, 'brickbakery_tile_4'),
+    (2, 0, 'brickbakery_tile_1'),
+    (2, 1, 'brickbakery_tile_2'),
+
+))
+layout_3 = Layout('layout_3', default_tile = 'brickbakery_tile_3', tiles = (
+    (0, 0, 'brickbakery_tile_3'),
+    (0, 1, 'brickbakery_tile_4'),
+    (0, 2, 'brickbakery_tile_3'),
+    (0, 3, 'brickbakery_tile_4'),
+    (1, 0, 'brickbakery_tile_1'),
+    (1, 1, 'brickbakery_tile_2'),
+    (1, 2, 'brickbakery_tile_1'),
+    (1, 3, 'brickbakery_tile_2'),
+))
 
 industry_name = 'grain_mill'
-industry = Industry(name=industry_name)
+industry = Industry(name=industry_name, layouts=(layout_1, layout_2, layout_3))
 
 # compile a single final nml file for the grf
 industry_template = industry_templates[industry_name + '.pypnml']
