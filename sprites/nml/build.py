b5586f11ef4c77bbd4bc45630b369422635a5407
Andy <andy@teamrubber.com>
2012-10-18 20:34:04 +0100

    Codechange: setup oil wells with stub of python templating

--- sprites/nml/build.py
+++ sprites/nml/build.py
@@ -17,3 +17,4 @@ sys.path.append(os.path.join('industries')) # add to the module search path
 
 from industries import food_market
 from industries import grain_mill
+from industries import oil_wells
