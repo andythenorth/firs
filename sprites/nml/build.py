fee5ddf8e9c86f432fe8db298a24e93102e2fb54
Andy <andy@teamrubber.com>
2012-10-20 20:35:21 +0100

    Codechange: convert biorefinery to stub of python templating

--- sprites/nml/build.py
+++ sprites/nml/build.py
@@ -15,6 +15,7 @@ currentdir = os.curdir
 import sys
 sys.path.append(os.path.join('industries')) # add to the module search path
 
+from industries import biorefinery
 from industries import bauxite_mine
 from industries import dredging_site
 from industries import food_market
