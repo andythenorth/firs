4ae1c8ac1fe7a46600646d0c11f1d30bebf8cb4b
Andy <andy@teamrubber.com>
2012-10-20 20:50:33 +0100

    Codechange: python template spritesets for biorefinery

--- sprites/nml/industries/biorefinery.py
+++ sprites/nml/industries/biorefinery.py
@@ -17,35 +17,49 @@ When a string is expected are basically two choices: provide a string directly, 
 
 industry = Industry(id='biorefinery')
 
-industry.add_tile(id='food_market_tile')
+industry.add_tile(id='biorefinery_tile')
 
 spriteset_ground = industry.add_spriteset(
-    id = 'food_market_spriteset_ground',
-    type='slab',
+    id = 'biorefinery_spriteset_ground',
+    type = 'concrete',
 )
 spriteset_ground_overlay = industry.add_spriteset(
-    id = 'food_market_spriteset_ground_overlay',
-    sprites = [(10, 10, 64, 31, -31, 0)],
+    id = 'biorefinery_spriteset_ground_overlay',
+    type = 'empty'
 )
 spriteset_1 = industry.add_spriteset(
-    id = 'food_market_spriteset',
-    sprites = [(10, 60, 64, 36, -31, -4)]
+    id = 'biorefinery_spriteset_1',
+    sprites = [(500, 10, 64, 66, -31, -35)]
 )
-sprite_tree_1 = industry.add_sprite(
-    sprite_number = 'nearby_tile_terrain_type(0, 0) != TILETYPE_SNOW ? market_tree : MARKET_TREE_SNOW', # defined in defines.pnml, to permit possibility of selecting a different sprite if a tree grf is loaded
-    xoffset= 11,
-    yoffset= 1,
-    xextent= 6,
-    yextent= 6
+spriteset_2 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_2',
+    sprites = [(570, 10, 64, 66, -31, -35)]
 )
-sprite_tree_2 = industry.add_sprite(
-    sprite_number = 'nearby_tile_terrain_type(0, 0) != TILETYPE_SNOW ? market_tree : MARKET_TREE_SNOW', # defined in defines.pnml, to permit possibility of selecting a different sprite if a tree grf is loaded
-    xoffset= 11,
-    yoffset= 6,
-    xextent= 6,
-    yextent= 6
+spriteset_3 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_3',
+    sprites = [(710, 10, 64, 66, -31, -35)]
 )
-
+spriteset_4 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_4',
+    sprites = [(80, 10, 64, 88, -31, -58)]
+)
+spriteset_5 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_5',
+    sprites = [(150, 10, 64, 88, -31, -59)]
+)
+spriteset_6 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_6',
+    sprites = [(220, 10, 64, 88, -31, -64)]
+)
+spriteset_7 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_7',
+    sprites = [(360, 10, 64, 73, -31, -45)]
+)
+spriteset_8 = industry.add_spriteset(
+    id = 'biorefinery_spriteset_8',
+    sprites = [(430, 10, 64, 66, -31, -38)]
+)
+"""
 industry.add_spritelayout(
     id = 'food_market_spritelayout',
     ground_sprite = spriteset_ground,
@@ -57,6 +71,6 @@ industry.add_industry_layout(
     default_spritelayout = 'food_market_spritelayout',
     layout = [(0, 0, 'food_market_tile', 'food_market_spritelayout')
     ]
-)
+)"""
 # Templating
 industry.render_and_save_pnml()
