0f803378fd6ed71424315c6dc15d470af437a19d
Andy <andy@teamrubber.com>
2012-10-28 07:39:53 +0000

    Codechange: use stub of python templating for brick works

--- sprites/nml/build.py
+++ sprites/nml/build.py
@@ -38,6 +38,7 @@ from industries import hardware_store
 
 # industries with only partial conversion to python templating, mostly still cpp
 from industries import arable_farm
+from industries import brick_works
 from industries import builders_yard
 from industries import cement_plant
 from industries import clay_pit
