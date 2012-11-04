5d0818fd4e42d9340c756c6c1c13ac4af7b578f2
Andy <andy@teamrubber.com>
2012-11-04 13:44:06 +0000

    Codechange: 'src/' is a better place than 'src'/nml/'

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -13,7 +13,7 @@ import os.path
 currentdir = os.curdir
 
 import sys
-sys.path.append(os.path.join(currentdir, 'src', 'nml')) # add to the module search path
+sys.path.append(os.path.join(currentdir, 'src')) # add to the module search path
 
 import codecs # used for writing files - more unicode friendly than standard open() module
 
