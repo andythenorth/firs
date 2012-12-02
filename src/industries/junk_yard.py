a1ed5207d23b14db115839ef9055585c669a808c
Andy <andy@teamrubber.com>
2012-12-02 13:57:11 +0000

    Change: partial snow for Junk Yard, also complete conversion to python templating

--- src/industries/junk_yard.py
+++ src/industries/junk_yard.py
@@ -22,7 +22,7 @@ industry = Industry(id='junk_yard',
                     input_multiplier_2='[0, 0]',
                     prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
                     prod_cargo_types='[SCMT]',
-                    layouts='[tilelayout_junkyard_1, tilelayout_junkyard_2, tilelayout_junkyard_3]',
+                    layouts='AUTO',
                     prob_in_game='3',
                     prob_random='7',
                     prod_multiplier='[12, 0]',
@@ -40,5 +40,155 @@ industry = Industry(id='junk_yard',
                     fund_cost_multiplier='101',
                     closure_msg='TTD_STR_NEWS_INDUSTRY_CLOSURE_SUPPLY_PROBLEMS')
 
-# industry uses layouts and sprites from default game, no custom layouts etc
+industry.add_tile(id='junk_yard_tile')
+
+sprite_ground = industry.add_sprite(
+    sprite_number = 'GROUNDTILE_MUD_TRACKS'
+)
+spriteset_ground_overlay = industry.add_spriteset(
+    id = 'junk_yard_spriteset_ground_overlay',
+    type = 'empty'
+)
+spriteset_1 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_1',
+    sprites = [(10, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_2 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_2',
+    sprites = [(80, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_3 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_3',
+    sprites = [(150, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_4 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_4',
+    sprites = [(220, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_5 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_5',
+    sprites = [(290, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_6 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_6',
+    sprites = [(360, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_7 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_7',
+    sprites = [(430, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_8 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_8',
+    sprites = [(500, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+spriteset_9 = industry.add_spriteset(
+    id = 'junk_yard_spriteset_9',
+    sprites = [(570, 10, 64, 55, -31, -24)],
+    zextent = 32
+)
+
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_1',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_1],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_2',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_2],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_3',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_3],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_4',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_4],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_5',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_5],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_6',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_6],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_7',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_7],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_8',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_8],
+)
+industry.add_spritelayout(
+    id = 'junk_yard_spritelayout_9',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_9],
+)
+
+industry.add_industry_layout(
+    id = 'junk_yard_industry_layout_1',
+    default_spritelayout = 'junk_yard_spritelayout_9',
+    layout = [(0, 2, 'junk_yard_tile', 'junk_yard_spritelayout_2'),
+              (1, 1, 'junk_yard_tile', 'junk_yard_spritelayout_2'),
+              (1, 2, 'junk_yard_tile', 'junk_yard_spritelayout_9'),
+              (2, 1, 'junk_yard_tile', 'junk_yard_spritelayout_1'),
+              (2, 2, 'junk_yard_tile', 'junk_yard_spritelayout_8'),
+              (3, 1, 'junk_yard_tile', 'junk_yard_spritelayout_4'),
+              (3, 2, 'junk_yard_tile', 'junk_yard_spritelayout_7'),
+              (4, 0, 'junk_yard_tile', 'junk_yard_spritelayout_5'),
+              (4, 1, 'junk_yard_tile', 'junk_yard_spritelayout_3'),
+              (4, 2, 'junk_yard_tile', 'junk_yard_spritelayout_6'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'junk_yard_industry_layout_2',
+    default_spritelayout = 'junk_yard_spritelayout_9',
+    layout = [(0, 1, 'junk_yard_tile', 'junk_yard_spritelayout_7'),
+              (1, 1, 'junk_yard_tile', 'junk_yard_spritelayout_1'),
+              (1, 2, 'junk_yard_tile', 'junk_yard_spritelayout_8'),
+              (2, 0, 'junk_yard_tile', 'junk_yard_spritelayout_5'),
+              (2, 1, 'junk_yard_tile', 'junk_yard_spritelayout_3'),
+              (2, 2, 'junk_yard_tile', 'junk_yard_spritelayout_6'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'junk_yard_industry_layout_3',
+    default_spritelayout = 'junk_yard_spritelayout_9',
+    layout = [(0, 3, 'junk_yard_tile', 'junk_yard_spritelayout_2'),
+              (1, 1, 'junk_yard_tile', 'junk_yard_spritelayout_2'),
+              (1, 3, 'junk_yard_tile', 'junk_yard_spritelayout_9'),
+              (2, 1, 'junk_yard_tile', 'junk_yard_spritelayout_1'),
+              (2, 3, 'junk_yard_tile', 'junk_yard_spritelayout_8'),
+              (3, 1, 'junk_yard_tile', 'junk_yard_spritelayout_4'),
+              (3, 3, 'junk_yard_tile', 'junk_yard_spritelayout_7'),
+              (4, 0, 'junk_yard_tile', 'junk_yard_spritelayout_5'),
+              (4, 1, 'junk_yard_tile', 'junk_yard_spritelayout_3'),
+              (4, 3, 'junk_yard_tile', 'junk_yard_spritelayout_6'),
+    ]
+)
 
