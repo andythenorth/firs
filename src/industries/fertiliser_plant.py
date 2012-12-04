9285a6f86c67f9762449fbb5bcfbbbb91ed7ef3a
Andy <andy@teamrubber.com>
2012-12-04 20:56:04 +0000

    Feature: snow for Fertiliser Plant, also fences and complete conversion to python templating (closes #1790) (closes #4589)

--- src/industries/fertiliser_plant.py
+++ src/industries/fertiliser_plant.py
@@ -22,7 +22,7 @@ industry = Industry(id='fertiliser_plant',
                     input_multiplier_2='[0, 0]',
                     prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
                     prod_cargo_types='[FMSP]',
-                    layouts='[tilelayout_fertiliser_plant_1, tilelayout_fertiliser_plant_2, tilelayout_fertiliser_plant_3, tilelayout_fertiliser_plant_4, tilelayout_fertiliser_plant_5, tilelayout_fertiliser_plant_6, tilelayout_fertiliser_plant_7]',
+                    layouts='AUTO',
                     prob_in_game='3',
                     prob_random='5',
                     prod_multiplier='[0, 0]',
@@ -40,5 +40,286 @@ industry = Industry(id='fertiliser_plant',
                     fund_cost_multiplier='170',
                     closure_msg='TTD_STR_NEWS_INDUSTRY_CLOSURE_SUPPLY_PROBLEMS')
 
-# industry uses layouts and sprites from default game, no custom layouts etc
 
+industry.add_tile(id='fertiliser_plant_tile')
+
+spriteset_ground = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_ground',
+    type='concrete',
+)
+spriteset_ground_overlay = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_ground_overlay',
+    type='empty',
+)
+
+spriteset_1 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_1',
+    sprites = [(80, 10, 64, 114, -31, -88)],
+    zextent = 48
+)
+spriteset_2 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_2',
+    sprites = [(150, 10, 64, 114, -31, -83)],
+    zextent = 48
+)
+spriteset_3 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_3',
+    sprites = [(220, 10, 64, 114, -31, -83)],
+    zextent = 48
+)
+spriteset_4 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_4',
+    sprites = [(290, 10, 64, 114, -31, -83)],
+    zextent = 48
+)
+spriteset_5 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_5',
+    sprites = [(360, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+spriteset_6 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_6',
+    sprites = [(430, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+spriteset_7 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_7',
+    sprites = [(500, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+spriteset_8 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_8',
+    sprites = [(570, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+spriteset_9 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_9',
+    sprites = [(640, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+spriteset_10 = industry.add_spriteset(
+    id = 'fertiliser_plant_spriteset_10',
+    sprites = [(710, 10, 64, 66, -31, -35)],
+    zextent = 48
+)
+sprite_smoke_1 = industry.add_smoke_sprite(
+    smoke_type = 'white_smoke_big',
+    xoffset= 5,
+    yoffset= 0,
+    zoffset= 69,
+    animation_frame_offset = 1
+)
+sprite_smoke_2 = industry.add_smoke_sprite(
+    smoke_type = 'white_smoke_big',
+    xoffset= 9,
+    yoffset= 0,
+    zoffset= 69,
+)
+
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_1',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_1],
+    smoke_sprites = [sprite_smoke_1, sprite_smoke_2],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_2',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_2],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_3',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_3],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_4',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_4],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_5',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_5],
+    fences = ['nw','ne']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_6',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_6],
+    fences = ['nw','ne']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_7',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_7],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_8',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_8],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_9',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_9],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_10',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_10],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'fertiliser_plant_spritelayout_concrete',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [],
+    fences = ['nw','ne','se','sw']
+)
+
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_1',
+    default_spritelayout = 'fertiliser_plant_spritelayout_8',
+    layout = [(0, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (0, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (0, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (1, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (2, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (3, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (3, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_concrete'),
+              (3, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_2',
+    default_spritelayout = 'fertiliser_plant_spritelayout_8',
+    layout = [(0, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (0, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (1, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (2, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (3, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (3, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (4, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (4, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_3',
+    default_spritelayout = 'fertiliser_plant_spritelayout_8',
+    layout = [(0, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (1, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (1, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+              (2, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (2, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_4',
+    default_spritelayout = 'fertiliser_plant_spritelayout_5',
+    layout = [(0, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (1, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (1, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (2, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (2, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_5',
+    default_spritelayout = 'fertiliser_plant_spritelayout_5',
+    layout = [(0, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (0, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (3, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (3, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (4, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (4, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (5, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (5, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (6, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+              (6, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_6',
+    default_spritelayout = 'fertiliser_plant_spritelayout_5',
+    layout = [(0, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (0, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (0, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (1, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (2, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (3, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (3, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (3, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_concrete'),
+              (4, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (4, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (4, 3, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+    ]
+)
+industry.add_industry_layout(
+    id = 'fertiliser_plant_industry_layout_7',
+    default_spritelayout = 'fertiliser_plant_spritelayout_5',
+    layout = [(0, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_4'),
+              (0, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (0, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_1'),
+              (0, 5, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (0, 6, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (1, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_3'),
+              (1, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (1, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (1, 5, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (1, 6, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_6'),
+              (2, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_2'),
+              (2, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_concrete'),
+              (2, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (2, 5, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+              (2, 6, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_5'),
+              (4, 0, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (4, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (4, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+              (4, 5, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (4, 6, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (5, 1, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_7'),
+              (5, 2, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_10'),
+              (5, 5, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_8'),
+              (5, 6, 'fertiliser_plant_tile', 'fertiliser_plant_spritelayout_9'),
+    ]
+)
