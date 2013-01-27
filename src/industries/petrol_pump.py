bb4519e35d53a801377439d89e9dde6a6cabd956
Andy <andy@teamrubber.com>
2013-01-27 19:23:17 +0000

    Change: don't set nearby station name for Petrol Pump (#4856)

--- src/industries/petrol_pump.py
+++ src/industries/petrol_pump.py
@@ -36,7 +36,6 @@ industry = Industry(id='petrol_pump',
                     remove_cost_multiplier='0',
                     prospect_chance='0.75',
                     name='string(STR_IND_PETROLPUMP)',
-                    nearby_station_name='string(STR_STATION, string(STR_TOWN), string(STR_IND_PETROLPUMP))',
                     fund_cost_multiplier='8',
                     closure_msg='TTD_STR_NEWS_INDUSTRY_CLOSURE_SUPPLY_PROBLEMS')
 
