8c88050764fc4e52c4bfbb9def18e2e6105f567e
Andy <andy@teamrubber.com>
2013-03-24 08:36:12 +0000

    Codechange: make cargo registering a little more modular

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -21,39 +21,7 @@ import codecs # used for writing files - more unicode friendly than standard ope
 
 import firs
 
-import cargos
-from cargos import alcohol
-from cargos import bauxite
-from cargos import building_materials
-from cargos import chemicals
-from cargos import clay
-from cargos import coal
-from cargos import engineering_supplies
-from cargos import farm_supplies
-from cargos import fish
-from cargos import food
-from cargos import fruits
-from cargos import goods
-from cargos import grain
-from cargos import gravel
-from cargos import iron_ore
-from cargos import livestock
-from cargos import lumber
-from cargos import mail
-from cargos import manufacturing_supplies
-from cargos import milk
-from cargos import oil
-from cargos import passengers
-from cargos import petrol
-from cargos import plant_fibres
-from cargos import recyclables
-from cargos import sand
-from cargos import scrap_metal
-from cargos import steel
-from cargos import sugar_beet
-from cargos import sugarcane
-from cargos import wood
-from cargos import wool
+import register_cargos
 
 firs.render_and_save_registered_cargos()
 
