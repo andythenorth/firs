bc2a36c9e5d42dd6928b86917cc00445f3f4d332
Andy <andy@teamrubber.com>
2013-07-20 12:28:08 +0100

    Codechange: rename master_control_program to firs for slightly less hacky makefile hack

--- src/render_docs.py
+++ src/render_docs.py
@@ -51,7 +51,7 @@ base_lang_strings = utils.parse_base_lang()
 metadata = {}
 metadata['dev_thread_url'] = 'http://www.tt-forums.net/viewtopic.php?t=41607'
 metadata['repo_url'] = 'http://dev.openttdcoop.org/projects/firs/repository'
-metadata['issue_tracker'] = 'http://dev.openttdcoop.org/projects/firs/repository'
+metadata['issue_tracker'] = 'http://dev.openttdcoop.org/projects/firs/issues'
 
 import firs
 from cargos import registered_cargos
