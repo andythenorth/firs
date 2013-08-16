5ef07b99357f390db981817f0a273a2283dc1203
Andy <andy@teamrubber.com>
2013-08-16 22:06:10 +0100

    Change: cargo icon for Rubber (#6002)

--- src/cargos/rubber.py
+++ src/cargos/rubber.py
@@ -20,7 +20,7 @@ cargo = Cargo(id = 'rubber',
               single_penalty_length = '36',
               capacity_multiplier = '1',
               price_factor = '107.634544373',
-              icon_indices = (3, 1))
+              icon_indices = (2, 2))
 
 cargo.economy_variations['FIRS']['disabled'] = True
 cargo.economy_variations['BASIC_TEMPERATE']['disabled'] = True
