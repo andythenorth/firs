a263a0e5186348a5efa0ef75acfd21e3188ccdf0
Andy <andy@teamrubber.com>
2015-01-22 13:09:50 +0000

    Change: Iron Works accepts sand, increase Iron Works total production for game balance, adjust Steel Mill production ratios (experimental) also for game balance

--- src/industries/iron_works.py
+++ src/industries/iron_works.py
@@ -16,7 +16,7 @@ When a string is expected are basically two choices: provide a string directly, 
 """
 
 industry = Industry(id='iron_works',
-                    accept_cargo_types=['IORE', 'WOOD'],
+                    accept_cargo_types=['IORE', 'WOOD', 'SAND'],
                     input_multiplier_1='[0, 0]',
                     input_multiplier_3='[0, 0]',
                     input_multiplier_2='[0, 0]',
