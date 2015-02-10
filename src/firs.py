7a8d63b7c544ac127fe58eebf96d96d3a3320a1e
Andy <andy@teamrubber.com>
2015-02-10 18:55:17 +0000

    Codechange: placate pyflakes by calling pointless asserts on industry modules after import

--- src/firs.py
+++ src/firs.py
@@ -72,58 +72,112 @@ from cargos import wool
 import industry
 from industries import registered_industries
 
+# keep these alphabetised for ease of reading
+# the asserts are just to stop pyflakes generating unhelpful warnings about unused imports
 from industries import aluminium_plant
+assert aluminium_plant
 from industries import arable_farm
+assert arable_farm
 from industries import basic_farm
+assert basic_farm
 from industries import bauxite_mine
+assert bauxite_mine
 from industries import biorefinery
+assert biorefinery
 from industries import brewery
+assert brewery
 from industries import brick_works
+assert brick_works
 from industries import builders_yard
+assert builders_yard
 from industries import bulk_terminal
+assert bulk_terminal
 from industries import cement_plant
+assert cement_plant
 from industries import chemical_plant
+assert chemical_plant
 from industries import coffee_estate
+assert coffee_estate
 from industries import copper_mine
+assert copper_mine
 from industries import dairy
+assert dairy
 from industries import dairy_farm
+assert dairy_farm
 from industries import diamond_mine
+assert diamond_mine
 from industries import dredging_site
+assert dredging_site
 from industries import fertiliser_plant
+assert fertiliser_plant
 from industries import fibre_crop_farm
+assert fibre_crop_farm
 from industries import food_market
+assert food_market
 from industries import food_processor
+assert food_processor
 from industries import furniture_factory
+assert furniture_factory
 from industries import general_store
+assert general_store
 from industries import glass_works
+assert glass_works
 from industries import grain_mill
+assert grain_mill
 from industries import hardware_store
+assert hardware_store
 from industries import hotel
+assert hotel
 from industries import iron_works
+assert iron_works
 from industries import junk_yard
+assert junk_yard
 from industries import lime_kiln
+assert lime_kiln
 from industries import lumber_yard
+assert lumber_yard
 from industries import machine_shop
+assert machine_shop
 from industries import metal_fabrication_plant
+assert metal_fabrication_plant
 from industries import metal_workshop
+assert metal_workshop
 from industries import mixed_farm
+assert mixed_farm
 from industries import nitrate_mine
+assert nitrate_mine
 from industries import oil_wells
+assert oil_wells
 from industries import orchard_piggery
+assert orchard_piggery
 from industries import petrol_pump
+assert petrol_pump
 from industries import plastics_plant
+assert plastics_plant
 from industries import ranch
+assert ranch
 from industries import recycling_depot
+assert recycling_depot
 from industries import recycling_plant
+assert recycling_plant
 from industries import rubber_plantation
+assert rubber_plantation
 from industries import sheep_farm
+assert sheep_farm
 from industries import smithy_forge
+assert smithy_forge
 from industries import stockyard
+assert stockyard
 from industries import sugar_plantation
+assert sugar_plantation
 from industries import sugar_refinery
+assert sugar_refinery
 from industries import textile_mill
+assert textile_mill
 from industries import trading_post
+assert trading_post
 from industries import vehicle_factory
+assert vehicle_factory
 
 # industries which will be hard to convert to python templating, mostly still cpp
 from industries import clay_pit
