7c4351a114208cd8ea136e2c2e29e3c95a8a6dca
Andy <andy@teamrubber.com>
2015-02-11 16:29:50 +0000

    Codechange: that shebang is probably unhelpful

--- src/render_docs.py
+++ src/render_docs.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
   This file is part of FIRS Industry Set for OpenTTD.
   FIRS is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
