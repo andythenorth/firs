aa118a7bc5a5b62065a696fa22e6363b72391416
Andy <andy@teamrubber.com>
2015-04-28 12:47:26 +0100

    Codechange: placate pyflakes

--- src/industries/iron_ore_mine.py
+++ src/industries/iron_ore_mine.py
@@ -5,7 +5,7 @@
   See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with FIRS. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from industry import IndustryPrimaryExtractive, TileLocationChecks, IndustryLocationChecks
+from industry import IndustryPrimaryExtractive
 
 industry = IndustryPrimaryExtractive(id='iron_ore_mine',
                     map_colour='55',
