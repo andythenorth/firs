0c47f06bbe19391b8c37aaf37c4014ecc99b3096
Andy <andy@teamrubber.com>
2015-04-30 20:49:06 +0100

    Codechange: convert Orchard Piggery to interim snakebite methods

--- src/industries/orchard_piggery.py
+++ src/industries/orchard_piggery.py
@@ -24,7 +24,8 @@ industry = IndustryPrimaryOrganic(id='orchard_piggery',
                     fund_cost_multiplier='54',
                     prod_multiplier='[6, 5]',
                     substitute='0',
-                    )
+                    template="refactor_orchard_piggery.pypnml",
+                    snakebite=True)
 
 industry.economy_variations['BASIC_TEMPERATE'].enabled = True
 
