dc2e0968c418acd45ebe4e3fe259909832913061
Andy <andy@teamrubber.com>
2015-05-20 21:53:33 +0100

    Codechange: move Coal Mine tiles and layouts to python

--- src/industries/coal_mine.py
+++ src/industries/coal_mine.py
@@ -8,90 +8,127 @@
 from industry import IndustryPrimaryExtractive, TileLocationChecks, IndustryLocationChecks
 
 industry = IndustryPrimaryExtractive(id='coal_mine',
-                    input_multiplier_1='[0, 0]',
+                    prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
+                    prod_cargo_types=['COAL'],
+                    layouts='AUTO',
                     prob_in_game='3',
                     prob_random='8',
                     prod_multiplier='[20, 0]',
-                    prod_cargo_types=['COAL'],
                     substitute='0',
+                    new_ind_msg='TTD_STR_NEWS_INDUSTRY_CONSTRUCTION',
                     map_colour='1',
+                    prod_decrease_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_DECREASE_GENERAL',
+                    min_cargo_distr='5',
+                    spec_flags='0',
                     location_checks=IndustryLocationChecks(require_cluster=['coal_mine', [20, 70, 2, 3]],
                                                            incompatible={'brick_works': 16,
                                                                          'lime_kiln': 16,
                                                                          'steel_mill': 16,
                                                                          'cement_plant': 16}),
+                    remove_cost_multiplier='0',
                     prospect_chance='0.75',
-                    nearby_station_name='string(STR_STATION, string(STR_TOWN), string(STR_STATION_MINE))',
                     name='TTD_STR_INDUSTRY_NAME_COAL_MINE',
+                    nearby_station_name='string(STR_STATION, string(STR_TOWN), string(STR_STATION_MINE))',
                     fund_cost_multiplier='252',
-                    override='0',
-                    template="refactor_coal_mine.pypnml" )
+                    closure_msg='TTD_STR_NEWS_INDUSTRY_CLOSURE_SUPPLY_PROBLEMS')
 
 industry.economy_variations['FIRS'].enabled = True
 industry.economy_variations['BASIC_TEMPERATE'].enabled = True
 
+industry.add_tile(id='coal_mine_tile_1',
+                  animation_length=3,
+                  animation_looping=True,
+                  animation_speed=1,
+                  custom_animation_control={'macro':'random_first_frame',
+                                            'animation_triggers': 'bitmask(ANIM_TRIGGER_INDTILE_CONSTRUCTION_STATE)'},
+                  location_checks=TileLocationChecks(disallow_slopes=True,
+                                                     disallow_industry_adjacent=True))
+
 sprite_ground = industry.add_sprite(
-    sprite_number = 'GROUNDTILE_MUD_TRACKS' # ground tile same as overlay tile
+    sprite_number = 'GROUNDTILE_MUD_TRACKS'
 )
-spriteset_ground_overlay = industry.add_spriteset(
-    id = 'coal_mine_spriteset_ground_overlay',
-    type = 'empty'
+sprite_ground_overlay = industry.add_sprite(
+    sprite_number = 'GROUNDTILE_MUD_TRACKS'
+)
+sprite_ground_coal = industry.add_sprite(
+    sprite_number = '2025'
 )
 
-spriteset_1a = industry.add_spriteset(
+spriteset_1 = industry.add_spriteset(
     id = 'coal_mine_spriteset_1a',
-    sprites = [(10, 10, 64, 110, -31, -70)],
-    zextent = 48
+    sprites = [(10, 10, 64, 110, -31, -70), (10, 130, 64, 110, -31, -70), (10, 250, 64, 110, -31, -70)],
+    zextent = 48,
+    animation_rate = 1
 )
-spriteset_1b = industry.add_spriteset(
-    id = 'coal_mine_spriteset_1b',
-    sprites = [(10, 130, 64, 110, -31, -70)],
-    zextent = 48
-)
-spriteset_1c = industry.add_spriteset(
-    id = 'coal_mine_spriteset_1c',
-    sprites = [(10, 250, 64, 110, -31, -70)],
-    zextent = 48
-)
-
 spriteset_2 = industry.add_spriteset(
     id = 'coal_mine_spriteset_2',
     sprites = [(80, 10, 64, 110, -31, -70)],
-    zextent = 48
+    zextent = 48,
+    num_sprites_to_autofill = len(spriteset_1.sprites), # autofills number of frames to match another spriteset which is animated etc (can get frame count from the other spriteset if defined already)
 )
 spriteset_3 = industry.add_spriteset(
     id = 'coal_mine_spriteset_3',
     sprites = [(150, 10, 64, 64, -31, -30)],
-    zextent = 48
+    zextent = 48,
+    num_sprites_to_autofill = len(spriteset_1.sprites), # autofills number of frames to match another spriteset which is animated etc (can get frame count from the other spriteset if defined already)
 )
 
 industry.add_spritelayout(
-    id = 'coal_mine_spritelayout_1a',
+    id = 'coal_mine_spritelayout_1',
     ground_sprite = sprite_ground,
-    ground_overlay = spriteset_ground_overlay,
-    building_sprites = [spriteset_1a],
-)
-industry.add_spritelayout(
-    id = 'coal_mine_spritelayout_1b',
-    ground_sprite = sprite_ground,
-    ground_overlay = spriteset_ground_overlay,
-    building_sprites = [spriteset_1b],
-)
-industry.add_spritelayout(
-    id = 'coal_mine_spritelayout_1c',
-    ground_sprite = sprite_ground,
-    ground_overlay = spriteset_ground_overlay,
-    building_sprites = [spriteset_1c],
+    ground_overlay = sprite_ground_overlay,
+    building_sprites = [spriteset_1],
 )
 industry.add_spritelayout(
     id = 'coal_mine_spritelayout_2',
     ground_sprite = sprite_ground,
-    ground_overlay = spriteset_ground_overlay,
+    ground_overlay = sprite_ground_overlay,
     building_sprites = [spriteset_2],
 )
 industry.add_spritelayout(
     id = 'coal_mine_spritelayout_3',
     ground_sprite = sprite_ground,
-    ground_overlay = spriteset_ground_overlay,
+    ground_overlay = sprite_ground_overlay,
     building_sprites = [spriteset_3],
+)
+industry.add_spritelayout(
+    id = 'coal_mine_spritelayout_4',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground_coal,
+    building_sprites = [],
+)
+
+industry.add_industry_layout(
+    id = 'coal_mine_industry_layout_1',
+    layout = [(0, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_1'),
+              (0, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_2'),
+              (0, 3, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (1, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (1, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_1'),
+              (1, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_2'),
+              (1, 3, 'coal_mine_tile_1', 'coal_mine_spritelayout_3'),
+              (2, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (2, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (2, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_3')
+    ]
+)
+industry.add_industry_layout(
+    id = 'coal_mine_industry_layout_2',
+    layout = [(0, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_1'),
+              (0, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_2'),
+              (0, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (1, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_3'),
+              (1, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_3'),
+              (1, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_4')
+    ]
+)
+industry.add_industry_layout(
+    id = 'coal_mine_industry_layout_3',
+    layout = [(0, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (1, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_4'),
+              (1, 1, 'coal_mine_tile_1', 'coal_mine_spritelayout_1'),
+              (1, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_2'),
+              (2, 0, 'coal_mine_tile_1', 'coal_mine_spritelayout_3'),
+              (2, 2, 'coal_mine_tile_1', 'coal_mine_spritelayout_3')
+    ]
 )
