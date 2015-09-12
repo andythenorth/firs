f794e63bad502d00abd397c47cc785c2e4dfda4c
Andy <andy@teamrubber.com>
2015-09-12 07:53:41 +0100

    Fix: (WIP) over-riding default power station didn't work, Power Plant tiles were accepting alcohol, instead re-implement the Power Plant spritesets, tile, etc in FIRS methods

--- src/industries/power_plant.py
+++ src/industries/power_plant.py
@@ -11,11 +11,11 @@ industry = IndustryTertiary(id='power_plant',
                     accept_cargo_types=['COAL'],
                     prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
                     prod_cargo_types=[],
-                    prob_in_game='12',
-                    prob_random='24',
+                    layouts='AUTO',
+                    prob_in_game='3',
+                    prob_random='5',
                     prod_multiplier='[0, 0]',
-                    substitute='1',
-                    override='1',
+                    substitute='0',
                     new_ind_msg='TTD_STR_NEWS_INDUSTRY_CONSTRUCTION',
                     map_colour='14',
                     prod_decrease_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_DECREASE_GENERAL',
@@ -34,28 +34,73 @@ industry = IndustryTertiary(id='power_plant',
 industry.economy_variations['FIRS'].enabled = True
 
 industry.add_tile(id='power_plant_tile_1',
-                  location_checks=TileLocationChecks(road_adjacent=['nw', 'ne', 'sw', 'se']))
+                  animation_length=7,
+                  animation_looping=True,
+                  animation_speed=3,
+                  custom_animation_control={'macro':'random_first_frame',
+                                            'animation_triggers': 'bitmask(ANIM_TRIGGER_INDTILE_CONSTRUCTION_STATE)'},
+                  location_checks=TileLocationChecks(require_effectively_flat=True,
+                                                     disallow_industry_adjacent=True))
+sprite_ground = industry.add_sprite(
+    sprite_number = 'GROUNDTILE_MUD_TRACKS'
+)
+sprite_ground_overlay = industry.add_sprite(
+    sprite_number = 'GROUNDTILE_MUD_TRACKS'
+)
+sprite_1 = industry.add_sprite(
+    sprite_number = '2047'
+)
+sprite_2 = industry.add_sprite(
+    sprite_number = '2050'
+)
+sprite_3 = industry.add_sprite(
+    sprite_number = '2053'
+)
+sprite_4 = industry.add_sprite(
+    sprite_number = '2054'
+)
+sprite_smoke_1 = industry.add_smoke_sprite(
+    smoke_type = 'white_smoke_big',
+    xoffset= 3,
+    yoffset= 0,
+    zoffset= 36
+)
 
-spriteset_ground = industry.add_spriteset(
-    id = 'power_plant_spriteset_ground',
-    type='slab',
-)
-spriteset_ground_overlay = industry.add_spriteset(
-    id = 'power_plant_spriteset_ground_overlay',
-    sprites = [(10, 10, 64, 31, -31, 0)],
-)
-spriteset_1 = industry.add_spriteset(
-    id = 'power_plant_spriteset',
-    sprites = [(10, 60, 64, 48, -31, -18)]
+industry.add_spritelayout(
+    id = 'power_plant_spritelayout_1',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground_overlay,
+    building_sprites = [sprite_1],
+    fences = ['nw','ne','se','sw']
 )
 industry.add_spritelayout(
-    id = 'power_plant_spritelayout',
-    ground_sprite = spriteset_ground,
-    ground_overlay = spriteset_ground_overlay,
-    building_sprites = [spriteset_1]
+    id = 'power_plant_spritelayout_2',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground_overlay,
+    building_sprites = [sprite_2],
+    smoke_sprites = [sprite_smoke_1],
+    fences = ['nw','ne','se','sw']
 )
+industry.add_spritelayout(
+    id = 'power_plant_spritelayout_3',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground_overlay,
+    building_sprites = [sprite_3],
+    fences = ['nw','ne','se','sw']
+)
+industry.add_spritelayout(
+    id = 'power_plant_spritelayout_4',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground_overlay,
+    building_sprites = [sprite_4],
+    fences = ['nw','ne','se','sw']
+)
+
 industry.add_industry_layout(
     id = 'power_plant_industry_layout',
-    layout = [(0, 0, 'power_plant_tile_1', 'power_plant_spritelayout')]
+    layout = [(0, 0, 'power_plant_tile_1', 'power_plant_spritelayout_1'),
+              (1, 0, 'power_plant_tile_1', 'power_plant_spritelayout_2'),
+              (2, 0, 'power_plant_tile_1', 'power_plant_spritelayout_3'),
+              (3, 0, 'power_plant_tile_1', 'power_plant_spritelayout_4')]
 )
 
