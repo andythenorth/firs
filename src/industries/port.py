aa11f178db24b73a66d42566d3b15a4f263531ef
Andy <andy@teamrubber.com>
2015-12-03 22:26:11 +0000

    Change: multiple industries and cargos added/updated

--- src/industries/port.py
+++ src/industries/port.py
@@ -52,8 +52,8 @@ industry.economy_variations['BASIC_TROPIC'].prod_cargo_types = ['ENSP', 'GOOD']
 industry.economy_variations['BASIC_TROPIC'].prod_multiplier = '[8, 17]'
 
 industry.economy_variations['MISTAH_KURTZ'].enabled = True
-industry.economy_variations['MISTAH_KURTZ'].accept_cargo_types = ['RUBR', 'COPR', 'FRUT']
-industry.economy_variations['MISTAH_KURTZ'].prod_cargo_types = ['ENSP', 'MNSP']
+industry.economy_variations['MISTAH_KURTZ'].accept_cargo_types = ['WDPR', 'COPR', 'FRUT']
+industry.economy_variations['MISTAH_KURTZ'].prod_cargo_types = ['VEHI', 'MNSP']
 
 industry.add_tile(id='port_tile_1',
                   land_shape_flags='bitmask(LSF_ONLY_ON_FLAT_LAND)',
