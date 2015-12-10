b7c6515b6da386e1819b46fceddb2f5024f624fb
Andy <andy@teamrubber.com>
2015-12-10 20:04:45 +0000

    merge

--- src/industries/port.py
+++ src/industries/port.py
@@ -42,7 +42,7 @@ industry.economy_variations['BASIC_TEMPERATE'].prod_cargo_types = ['ENSP']
 industry.economy_variations['BASIC_TEMPERATE'].prod_multiplier = '[19, 0]'
 
 industry.economy_variations['BASIC_ARCTIC'].enabled = True
-industry.economy_variations['BASIC_ARCTIC'].accept_cargo_types = ['PAPR', 'VEHI', 'ENSP']
+industry.economy_variations['BASIC_ARCTIC'].accept_cargo_types = ['PAPR', 'VEHI']
 industry.economy_variations['BASIC_ARCTIC'].prod_cargo_types = ['FOOD', 'RUBR']
 industry.economy_variations['BASIC_ARCTIC'].prod_multiplier = '[7, 16]'
 
