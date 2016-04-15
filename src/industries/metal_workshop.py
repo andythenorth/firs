01aafd2ec0ef4ed2a4af45701dd8df0853912c13
Andy <andy@teamrubber.com>
2016-04-15 06:21:53 +0100

    Fix: Metal Workshop cargo ratio was wrong

--- src/industries/metal_workshop.py
+++ src/industries/metal_workshop.py
@@ -8,7 +8,7 @@
 from industry import IndustrySecondary, TileLocationChecks, IndustryLocationChecks
 
 industry = IndustrySecondary(id='metal_workshop',
-                    processed_cargos_and_output_ratios=[('STEL', 2), ('RFPR', 2)],
+                    processed_cargos_and_output_ratios=[('STEL', 6), ('RFPR', 2)],
                     combined_cargos_boost_prod=True,
                     prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
                     prod_cargo_types=['GOOD', 'MNSP'],
