cccac9137dbe916a9907258aafcecc8f88854d93
Andy <andy@teamrubber.com>
2016-12-08 19:42:29 +0000

    Change: adjust bulk terminal production levels in Steeltown

--- src/industries/bulk_terminal.py
+++ src/industries/bulk_terminal.py
@@ -59,7 +59,7 @@ industry.economy_variations['MISTAH_KURTZ'].prod_multiplier = '[12, 12]'
 industry.economy_variations['STEELTOWN'].enabled = True
 industry.economy_variations['STEELTOWN'].accept_cargo_types = ['FMSP', 'SESP']
 industry.economy_variations['STEELTOWN'].prod_cargo_types = ['BASE', 'RUBR']
-industry.economy_variations['STEELTOWN'].prod_multiplier = '[7, 19]'
+industry.economy_variations['STEELTOWN'].prod_multiplier = '[16, 16]'
 
 industry.add_tile(id='bulk_terminal_tile_1',
                   land_shape_flags='bitmask(LSF_ONLY_ON_FLAT_LAND)',
