9054e84b3e8368203ad95a70389fd0a2a1ddae70
Andy <andy@teamrubber.com>
2016-12-15 20:33:53 +0000

    Fix: update accepted cargo for industries that should now accept METL not STEL

--- src/industries/smithy_forge.py
+++ src/industries/smithy_forge.py
@@ -8,7 +8,7 @@
 from industry import IndustrySecondary, TileLocationChecks, IndustryLocationChecks
 
 industry = IndustrySecondary(id='smithy_forge',
-                    processed_cargos_and_output_ratios=[('STEL', 8)],
+                    processed_cargos_and_output_ratios=[('METL', 8)],
                     prod_increase_msg='TTD_STR_NEWS_INDUSTRY_PRODUCTION_INCREASE_GENERAL',
                     prod_cargo_types=['ENSP', 'FMSP'],
                     layouts='AUTO',
