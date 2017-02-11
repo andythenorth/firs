d4c7ace140368afde1076928302f85a65a4aa90a
Andy <andy@teamrubber.com>
2017-02-11 21:13:11 +0000

    Change: try 'vehicle bodies' instead of 'galvanised steel'

--- src/industries/metal_fabrication_plant.py
+++ src/industries/metal_fabrication_plant.py
@@ -36,7 +36,6 @@ industry.economy_variations['FIRS'].enabled = True
 industry.economy_variations['STEELTOWN'].enabled = True
 industry.economy_variations['STEELTOWN'].processed_cargos_and_output_ratios = [('STEL', 4), ('NFML', 4), ('RFPR', 2)]
 industry.economy_variations['STEELTOWN'].prod_cargo_types = ['GALV', 'BDMT']
-industry.economy_variations['STEELTOWN'].name = 'string(STR_IND_GALVANISING_PLANT)'
 
 industry.add_tile(id='metal_fabrication_plant_tile_1',
                   animation_length=71,
