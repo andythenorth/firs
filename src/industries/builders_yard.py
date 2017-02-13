fc1ee2fb8360ee4ccaaadf128de234704bdbe87c
Andy <andy@teamrubber.com>
2017-02-13 20:55:29 +0000

    Change: (experimental) are builders yards needed in Steeltown? o_O

--- src/industries/builders_yard.py
+++ src/industries/builders_yard.py
@@ -33,8 +33,6 @@ industry = IndustryTertiary(id='builders_yard',
 industry.economy_variations['FIRS'].enabled = True
 industry.economy_variations['MISTAH_KURTZ'].enabled = True
 industry.economy_variations['MISTAH_KURTZ'].prob_random = '14'
-industry.economy_variations['STEELTOWN'].enabled = True
-industry.economy_variations['STEELTOWN'].accept_cargo_types = ['BDMT']
 
 industry.add_tile(id='builders_yard_tile_1',
                   location_checks=TileLocationChecks(require_houses_nearby=True,
