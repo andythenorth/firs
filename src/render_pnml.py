98b4c4ce777cea1ddd63da702da8eb8166eca76e
Andy <andy@teamrubber.com>
2017-02-28 10:46:18 +0000

    Codechange: no CPP in header template, rename to .pynml

--- src/render_pnml.py
+++ src/render_pnml.py
@@ -51,12 +51,14 @@ def render_industry(industry):
 
 def main():
     start = time()
-    header_items = ['defines', 'checks','header','firs','parameters']
+    # extension is included due to partial migration from pypnaml to pynml; it should ideally be standard and concatenated within the repeat
+    header_items = ['defines.pypnml', 'checks.pypnml','header.pynml','firs.pypnml','parameters.pypnml']
     for header_item in header_items:
-        template = templates[header_item + '.pypnml']
+        template = templates[header_item]
         templated_pnml = utils.unescape_chameleon_output(template(registered_industries=registered_industries,
                                                                   global_constants=global_constants,
                                                                   economies=registered_economies,
+                                                                  repo_vars=repo_vars,
                                                                   utils=utils,
                                                                   sys=sys,
                                                                   generated_pnml_path=generated_pnml_path))
