9ed0a0b0b168a730914921e471c1ab839800c0ca
Andy <andy@teamrubber.com>
2017-04-14 15:12:11 +0100

    Codechange: reimplement Oil Rig using FIRS spritelayouts etc

--- src/industries/oil_rig.py
+++ src/industries/oil_rig.py
@@ -1,7 +1,8 @@
-from industry import IndustryPrimaryExtractive
+from industry import IndustryPrimaryExtractive, TileLocationChecks
 
 industry = IndustryPrimaryExtractive(id='oil_rig',
                     prod_cargo_types=['OIL_', 'PASS'],
+                    layouts='AUTO',
                     prob_in_game='6',
                     prob_random='6',
                     prod_multiplier='[29, 4]',
@@ -14,21 +15,101 @@ industry = IndustryPrimaryExtractive(id='oil_rig',
                     nearby_station_name='string(STR_STATION, string(STR_TOWN), string(STR_STATION_WATER))',
                     fund_cost_multiplier='255',
                     override='5',
-                    template="refactor/refactor_oil_rig.pynml",
                     intro_year=1967)
 
 industry.economy_variations['FIRS'].enabled = True
 industry.economy_variations['MISTAH_KURTZ'].enabled = True
 
-# industry uses layouts and sprites from default game, no custom layouts etc
-"""
-To-do
-- put tile in
-- get sprite numbers of default sprites using in-game tools
-- put spritesets in
-- put spritelayouts in
-- figure out where the station tiles are
-- put industry layouts in, as per https://newgrf-specs.tt-wiki.net/wiki/IndustryDefaultProps
-- set layouts flag to 'AUTO'
-- remove custom template from filesystem, and the property declaring it here
-"""
+industry.add_tile(id='oil_rig_tile_1',
+                  location_checks=TileLocationChecks(disallow_industry_adjacent=True))
+
+sprite_ground = industry.add_sprite(
+    sprite_number = 'GROUNDSPRITE_WATER',
+)
+spriteset_ground_empty = industry.add_spriteset(
+    id = 'oil_rig_spriteset_ground_empty',
+    type = 'empty'
+)
+sprite_1 = industry.add_sprite(
+    sprite_number = '2096'
+)
+sprite_2 = industry.add_sprite(
+    sprite_number = '2097'
+)
+sprite_3 = industry.add_sprite(
+    sprite_number = '2098'
+)
+sprite_4 = industry.add_sprite(
+    sprite_number = '2099'
+)
+
+industry.add_spritelayout(
+    id = 'oil_rig_spritelayout_1',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_empty,
+    building_sprites = [sprite_1]
+)
+industry.add_spritelayout(
+    id = 'oil_rig_spritelayout_2',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_empty,
+    building_sprites = [sprite_2]
+)
+industry.add_spritelayout(
+    id = 'oil_rig_spritelayout_3',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_empty,
+    building_sprites = [sprite_3]
+)
+industry.add_spritelayout(
+    id = 'oil_rig_spritelayout_4',
+    ground_sprite = sprite_ground,
+    ground_overlay = spriteset_ground_empty,
+    building_sprites = [sprite_4]
+)
+industry.add_spritelayout(
+    id = 'oil_rig_spritelayout_null',
+    ground_sprite = sprite_ground,
+    ground_overlay = sprite_ground,
+    building_sprites = []
+)
+
+industry.add_industry_layout(
+    id = 'oil_rig_layout_1',
+    layout = [(0, 0, '255', 'oil_rig_spritelayout_null'),
+              (0, 1, '255', 'oil_rig_spritelayout_null'),
+              (0, 2, '255', 'oil_rig_spritelayout_null'),
+              (0, 3, '255', 'oil_rig_spritelayout_null'),
+              (0, 4, '255', 'oil_rig_spritelayout_null'),
+              (0, 5, '255', 'oil_rig_spritelayout_null'),
+              (0, 6, '255', 'oil_rig_spritelayout_null'),
+              (0, 7, '255', 'oil_rig_spritelayout_null'),
+              (0, 8, '255', 'oil_rig_spritelayout_null'),
+              (0, 9, '255', 'oil_rig_spritelayout_null'),
+              (0, 10, '255', 'oil_rig_spritelayout_null'),
+              (1, 0, '255', 'oil_rig_spritelayout_null'),
+              (1, 10, '255', 'oil_rig_spritelayout_null'),
+              (2, 0, '255', 'oil_rig_spritelayout_null'),
+              (2, 10, '255', 'oil_rig_spritelayout_null'),
+              (3, 0, '255', 'oil_rig_spritelayout_null'),
+              (3, 10, '255', 'oil_rig_spritelayout_null'),
+              (4, 0, '255', 'oil_rig_spritelayout_null'),
+              (4, 3, '24', 'oil_rig_spritelayout_null'),
+              (4, 4, '24', 'oil_rig_spritelayout_null'),
+              (4, 5, 'oil_rig_tile_1', 'oil_rig_spritelayout_4'),
+              (4, 10, '255', 'oil_rig_spritelayout_null'),
+              (5, 0, '255', 'oil_rig_spritelayout_null'),
+              (5, 5, 'oil_rig_tile_1', 'oil_rig_spritelayout_3'),
+              (5, 4, 'oil_rig_tile_1', 'oil_rig_spritelayout_2'),
+              (5, 3, 'oil_rig_tile_1', 'oil_rig_spritelayout_1'),
+              (5, 10, '255', 'oil_rig_spritelayout_null'),
+              (6, 0, '255', 'oil_rig_spritelayout_null'),
+              (6, 10, '255', 'oil_rig_spritelayout_null'),
+              (7, 0, '255', 'oil_rig_spritelayout_null'),
+              (7, 10, '255', 'oil_rig_spritelayout_null'),
+              (8, 0, '255', 'oil_rig_spritelayout_null'),
+              (8, 10, '255', 'oil_rig_spritelayout_null'),
+              (9, 0, '255', 'oil_rig_spritelayout_null'),
+              (9, 10, '255', 'oil_rig_spritelayout_null'),
+    ]
+)
