96a2ee7421062425b34ed69510b8c5ba03ae2a0b
Andy <andy@teamrubber.com>
2017-04-14 16:24:31 +0100

    Codechange: (lang) remove dead string

--- scripts/remove_dead_lang_strings.py
+++ scripts/remove_dead_lang_strings.py
@@ -10,15 +10,14 @@ sys.path.append(os.path.join('src')) # add to the module search path
 # this script removes dead strings - adjust the 'dead_strings' list to suit
 
 # never leave empty strings or strings with only spaces in this list, that will strip everything from a lang file
-dead_strings = ["STR_CARGO_NAME_SUGARCANE",
-                "STR_CARGO_UNIT_SUGARCANE",
+dead_strings = ["STR_IND_FERTILISER_PLANT",
                 "STR_CID_SUGARCANE"]
 
 def delete_string(dead_string):
-    for filename in os.listdir(os.path.join('lang')):
+    for filename in os.listdir(os.path.join('src','lang')):
         print(filename)
         if filename is not '.DS_Store':
-            file = codecs.open(os.path.join('lang', filename),'r', encoding='utf-8')
+            file = codecs.open(os.path.join('src','lang', filename),'r', encoding='utf-8')
             content = file.readlines()
             result = []
 
@@ -26,7 +25,7 @@ def delete_string(dead_string):
                 if dead_string not in line:
                     result.append(line)
 
-            file = open(os.path.join('lang',filename),'w')
+            file = open(os.path.join('src','lang',filename),'w')
             for line in result:
                 file.write(line)
             file.close
