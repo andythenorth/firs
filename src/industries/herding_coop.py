e71545406bf52ce028d473c4fa02ad2e420c8c60
Andy <andy@teamrubber.com>
2017-05-02 09:59:01 +0100

    Change: improved appearance for Herding Co-op

--- src/industries/herding_coop.py
+++ src/industries/herding_coop.py
@@ -33,43 +33,72 @@ spriteset_ground_overlay = industry.add_spriteset(
 spriteset_1 = industry.add_spriteset(
     sprites = [(10, 10, 64, 52, -31, -21)],
 )
-spriteset_1_ground = industry.add_spriteset(
-    sprites = [(10, 70, 64, 52, -31, -21)],
-)
 spriteset_2 = industry.add_spriteset(
     sprites = [(80, 10, 64, 52, -31, -21)],
 )
-spriteset_2_ground = industry.add_spriteset(
-    sprites = [(80, 70, 64, 52, -31, -21)],
+spriteset_3 = industry.add_spriteset(
+    sprites = [(150, 10, 64, 52, -31, -21)],
+)
+spriteset_4 = industry.add_spriteset(
+    sprites = [(220, 10, 64, 52, -31, -21)],
+)
+spriteset_5 = industry.add_spriteset(
+    sprites = [(290, 10, 64, 52, -31, -21)],
+)
+spriteset_6 = industry.add_spriteset(
+    sprites = [(360, 10, 64, 52, -31, -21)],
 )
 
 industry.add_spritelayout(
-    id = 'herding_coop_spritelayout_1',
+    id = 'herding_coop_spritelayout_large_hut',
     ground_sprite = spriteset_ground,
-    ground_overlay = spriteset_1_ground,
+    ground_overlay = spriteset_ground_overlay,
     building_sprites = [spriteset_1],
     terrain_aware_ground = True
 )
 industry.add_spritelayout(
-    id = 'herding_coop_spritelayout_2',
+    id = 'herding_coop_spritelayout_brown_hut',
     ground_sprite = spriteset_ground,
-    ground_overlay = spriteset_2_ground,
+    ground_overlay = spriteset_ground_overlay,
     building_sprites = [spriteset_2],
     terrain_aware_ground = True
 )
 industry.add_spritelayout(
-    id = 'herding_coop_spritelayout_3',
+    id = 'herding_coop_spritelayout_two_brown_huts',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_3],
+    terrain_aware_ground = True
+)
+industry.add_spritelayout(
+    id = 'herding_coop_spritelayout_paddock_1',
     ground_sprite = sprite_ground_mud,
     ground_overlay = sprite_ground_mud,
-    building_sprites = [],
+    building_sprites = [spriteset_4],
+    terrain_aware_ground = True
+)
+industry.add_spritelayout(
+    id = 'herding_coop_spritelayout_paddock_2',
+    ground_sprite = sprite_ground_mud,
+    ground_overlay = sprite_ground_mud,
+    building_sprites = [spriteset_5],
+    terrain_aware_ground = True
+)
+industry.add_spritelayout(
+    id = 'herding_coop_spritelayout_bare_ground',
+    ground_sprite = spriteset_ground,
+    ground_overlay = spriteset_ground_overlay,
+    building_sprites = [spriteset_6],
     terrain_aware_ground = True
 )
 
 industry.add_industry_layout(
     id = 'herding_coop_industry_layout_1',
-    layout = [(0, 1, 'herding_coop_tile_1', 'herding_coop_spritelayout_3'),
-              (0, 2, 'herding_coop_tile_1', 'herding_coop_spritelayout_1'),
-              (1, 0, 'herding_coop_tile_1', 'herding_coop_spritelayout_2'),
-              (1, 2, 'herding_coop_tile_1', 'herding_coop_spritelayout_2'),
+    layout = [(0, 0, 'herding_coop_tile_1', 'herding_coop_spritelayout_paddock_2'),
+              (0, 1, 'herding_coop_tile_1', 'herding_coop_spritelayout_paddock_1'),
+              (0, 2, 'herding_coop_tile_1', 'herding_coop_spritelayout_large_hut'),
+              (1, 0, 'herding_coop_tile_1', 'herding_coop_spritelayout_brown_hut'),
+              (1, 1, 'herding_coop_tile_1', 'herding_coop_spritelayout_two_brown_huts'),
+              (1, 2, 'herding_coop_tile_1', 'herding_coop_spritelayout_bare_ground'),
     ]
 )
