f1da80b2b9479539503b5c020b5cf9a3dcab52d0
Andy <andy@teamrubber.com>
2017-07-08 10:50:58 +0100

    Codechange: comment that economy parameter order can't use indirection for economy numeric_id value

--- src/economies/__init__.py
+++ src/economies/__init__.py
@@ -2,6 +2,7 @@ registered_economies = []
 
 # specify economies in the order that they should appear in parameter list in-game (and also in docs)
 # economies have a numeric ID which maps parameter values and avoids breaking savegames when this list changes
+# !! ^ that doesn't appear to work, action 14 param doesn't seem to be able to abstract name value from name orde??
 
 from economies import basic_temperate
 basic_temperate.economy.register()
@@ -12,11 +13,11 @@ basic_arctic.economy.register()
 from economies import basic_tropic
 basic_tropic.economy.register()
 
+from economies import steeltown
+steeltown.economy.register()
+
 from economies import mistah_kurtz
 mistah_kurtz.economy.register()
 
 from economies import firs
 firs.economy.register()
-
-from economies import steeltown
-steeltown.economy.register()
