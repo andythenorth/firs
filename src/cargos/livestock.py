c7268739e3536d67d9a01981078273e245b615aa
Andy <andy@teamrubber.com>
2017-07-09 21:38:39 +0100

    Codechange: no-eating-ponies comment

--- src/cargos/livestock.py
+++ src/cargos/livestock.py
@@ -5,7 +5,7 @@ cargo = Cargo(id = 'livestock',
               unit_name = 'TTD_STR_CARGO_SINGULAR_LIVESTOCK',
               type_abbreviation = 'TTD_STR_ABBREV_LIVESTOCK',
               sprite = 'NEW_CARGO_SPRITE',
-              weight = '0.1875', # average weight of a shetland pony apparently
+              weight = '0.1875', # average weight of a shetland pony apparently (and no we don't eat ponies, but eh)
               cargo_payment_list_colour = '75',
               is_freight = '1',
               cargo_classes = 'bitmask(CC_PIECE_GOODS)',
