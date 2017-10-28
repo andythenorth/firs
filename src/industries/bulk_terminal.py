69c3720abb8791a78f08e7e282829de4cf941560
Andy <andy@teamrubber.com>
2017-10-28 07:25:24 +0100

    Fix: remove unused powertrain cargo entry from Steeltown cargos, broke compile

--- src/industries/bulk_terminal.py
+++ src/industries/bulk_terminal.py
@@ -42,7 +42,7 @@ industry.economy_variations['MISTAH_KURTZ'].prod_cargo_types = ['RFPR', 'FMSP']
 industry.economy_variations['MISTAH_KURTZ'].prod_multiplier = '[12, 12]'
 
 industry.economy_variations['STEELTOWN'].enabled = True
-industry.economy_variations['STEELTOWN'].accept_cargo_types = ['SULP']
+industry.economy_variations['STEELTOWN'].accept_cargo_types = ['GRAI']
 industry.economy_variations['STEELTOWN'].prod_cargo_types = ['MNO2', 'LIME']
 industry.economy_variations['STEELTOWN'].prod_multiplier = '[19, 12]'
 
--- src/industries/port.py
+++ src/industries/port.py
@@ -41,7 +41,7 @@ industry.economy_variations['MISTAH_KURTZ'].prod_cargo_types = ['ENSP', 'GOOD']
 industry.economy_variations['MISTAH_KURTZ'].prod_multiplier = '[17, 14]'
 
 industry.economy_variations['STEELTOWN'].enabled = True
-industry.economy_variations['STEELTOWN'].accept_cargo_types = ['FOOD']
+industry.economy_variations['STEELTOWN'].accept_cargo_types = ['FRUT']
 industry.economy_variations['STEELTOWN'].prod_cargo_types = ['ZINC', 'COPR']
 industry.economy_variations['STEELTOWN'].prod_multiplier = '[14]'
 
