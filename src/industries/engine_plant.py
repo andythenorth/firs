a0f3faa1c32e65c96e48952c51a16b5dd4145455
Andy <andy@teamrubber.com>
2020-04-05 20:58:21 +0100

    Change: minor nerf to engine plant production, for balance

--- src/industries/engine_plant.py
+++ src/industries/engine_plant.py
@@ -3,7 +3,7 @@ from industry import IndustrySecondary, TileLocationChecks
 industry = IndustrySecondary(id='engine_plant',
                              accept_cargos_with_input_ratios=[('CSTI', 4), ('ALUM', 2), ('SAND', 2)],
                              combined_cargos_boost_prod=True,
-                             prod_cargo_types_with_output_ratios=[('VENG', 8)],
+                             prod_cargo_types_with_output_ratios=[('VENG', 6)], # high engine plant production is unwanted as there is only one output cargo
                              prob_in_game='3',
                              prob_map_gen='5',
                              map_colour='45',
