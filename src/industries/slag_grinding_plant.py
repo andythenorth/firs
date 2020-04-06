4903162131b5ae48b32463dd95f2c4b0ecc46c7a
Andy <andy@teamrubber.com>
2020-04-06 22:10:42 +0100

    Change: adjust production ratios at Slag Grinding Plant to ease cargo supply to Wharf

--- src/industries/slag_grinding_plant.py
+++ src/industries/slag_grinding_plant.py
@@ -2,7 +2,7 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(id='slag_grinding_plant',
                              accept_cargos_with_input_ratios=[('SLAG', 8)],
-                             prod_cargo_types_with_output_ratios=[('CMNT', 4), ('FMSP', 4)],
+                             prod_cargo_types_with_output_ratios=[('CMNT', 5), ('FMSP', 3)],
                              prob_in_game='3',
                              prob_map_gen='5',
                              map_colour='19',
