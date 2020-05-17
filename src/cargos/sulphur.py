ea1e8a0ed33eec3af023ac00c44d453237984490
Andy <andy@teamrubber.com>
2020-05-17 20:28:48 +0100

    Change: slightly decrease sulphur cargo price to make room for sulphuric acid in BLTC

--- src/cargos/sulphur.py
+++ src/cargos/sulphur.py
@@ -15,6 +15,6 @@ cargo = Cargo(id='sulphur',
               items_of_cargo='string(STR_CARGO_UNIT_SULPHUR)',
               penalty_lowerbound='30',
               single_penalty_length='255',
-              price_factor=102,
+              price_factor=98,
               capacity_multiplier='1',
               icon_indices=(13, 3))
