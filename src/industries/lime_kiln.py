e14b7369d794c8d13a7311d422b84399330b8608
Andy <andy@teamrubber.com>
2020-10-20 10:01:00 +0100

    Change: produce FMSP at Lime Kiln as extra FMSP source in Steeltown - aids Food -> Bulk Terminal flow

--- src/industries/lime_kiln.py
+++ src/industries/lime_kiln.py
@@ -1,9 +1,9 @@
 from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(id='lime_kiln',
-                             accept_cargos_with_input_ratios=[('LIME', 8)],
+                             accept_cargos_with_input_ratios=[('LIME', 8)], # no fuel source, at least in Steeltown, it's better for gameplay - assume natural gas fuel
                              combined_cargos_boost_prod=True,
-                             prod_cargo_types_with_output_ratios=[('QLME', 6)],
+                             prod_cargo_types_with_output_ratios=[('QLME', 6), ('FMSP', 2)],
                              prob_in_game='3',
                              prob_map_gen='5',
                              map_colour='119',
