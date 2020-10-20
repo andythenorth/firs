e5598fccd294bae98c63c13b0eafb1ed1df03c99
Andy <andy@teamrubber.com>
2020-10-20 21:48:53 +0100

    Change: remove Limestone from Glass Works, too much detail, not the main thing for Steeltown

--- src/industries/glass_works.py
+++ src/industries/glass_works.py
@@ -23,7 +23,7 @@ industry.economy_variations['IN_A_HOT_COUNTRY'].accept_cargos_with_input_ratios 
 industry.economy_variations['IN_A_HOT_COUNTRY'].prod_cargo_types_with_output_ratios = [('BDMT', 4), ('GOOD', 4)]
 
 industry.economy_variations['STEELTOWN'].enabled = True
-industry.economy_variations['STEELTOWN'].accept_cargos_with_input_ratios = [('SAND', 4), ('SASH', 2), ('LIME', 2)]
+industry.economy_variations['IN_A_HOT_COUNTRY'].accept_cargos_with_input_ratios = [('SAND', 6), ('RFPR', 2)] # no limestone or dolomite, too much detail
 
 industry.add_tile(id='glass_works_tile_1',
                   animation_length=71,
