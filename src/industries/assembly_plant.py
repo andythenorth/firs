a576f6e267eb950fd65f9c043a2f6f8d6410d046
Andy <andy@teamrubber.com>
2020-10-27 19:42:38 +0000

    Change: low probability of building Assembly Plant in-game for Steeltown, rather than zero probability

--- src/industries/assembly_plant.py
+++ src/industries/assembly_plant.py
@@ -12,7 +12,7 @@ industry = IndustrySecondary(id='assembly_plant',
                              fund_cost_multiplier='145')
 
 industry.economy_variations['STEELTOWN'].enabled = True
-industry.economy_variations['STEELTOWN'].prob_in_game = '0' # do not build during gameplay
+industry.economy_variations['STEELTOWN'].prob_in_game = '1' # low chance of build during gameplay
 
 industry.add_tile(id='assembly_plant_tile_1',
                   animation_length=71,
