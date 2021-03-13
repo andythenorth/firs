9b3d23429744dbdb6ca0967d299454be45e768cd
Andy <andy@teamrubber.com>
2021-03-13 07:35:37 +0000

    Fix: RFPR not COAT for In A Hot Country Timber Yard

--- src/industries/lumber_yard.py
+++ src/industries/lumber_yard.py
@@ -16,6 +16,10 @@ industry = IndustrySecondary(
 ###industry.economy_variations['BETTER_LIVING_THROUGH_CHEMISTRY'].enabled = True
 
 industry.economy_variations["IN_A_HOT_COUNTRY"].enabled = True
+industry.economy_variations["IN_A_HOT_COUNTRY"].accept_cargos_with_input_ratios = [
+    ("WDPR", 6),
+    ("RFPR", 2),
+]
 industry.economy_variations["IN_A_HOT_COUNTRY"].prod_cargo_types_with_output_ratios = [
     ("BDMT", 8)
 ]
