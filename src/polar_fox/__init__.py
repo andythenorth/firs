e81faed203051c174304e809433416b044c9149f
Andy <andy@teamrubber.com>
2021-06-05 15:58:35 +0100

    Codechange: updated Polar Fox

--- src/polar_fox/__init__.py
+++ src/polar_fox/__init__.py
@@ -3,3 +3,4 @@ This file is generated from the Polar Fox project.
 Don't make changes here, make them in the Polar Fox project and redistribute.
 Any changes made here are liable to be over-written.
 """
+
