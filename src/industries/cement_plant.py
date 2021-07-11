f6f8aa0ed3c3546d6b9ee2e2f0185d8e89a6e5ca
Andy <andy@teamrubber.com>
2021-07-11 09:47:39 +0100

    Codechange: blacken src

--- src/industries/cement_plant.py
+++ src/industries/cement_plant.py
@@ -10,7 +10,10 @@ industry = IndustrySecondary(
     map_colour="19",
     # the keystones are quite specific to IAHC, and location checks aren't economy specific, so this might need adjusted if other economies gain cement plant
     location_checks=dict(
-        near_at_least_one_of_these_keystone_industries=[["quarry", "phosphate_mine"], 96],
+        near_at_least_one_of_these_keystone_industries=[
+            ["quarry", "phosphate_mine"],
+            96,
+        ],
         same_type_distance=96,
     ),
     special_flags=["IND_FLAG_MILITARY_HELICOPTER_CAN_EXPLODE"],
