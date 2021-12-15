8cc1659c5ac62e9c53644c93d1fdcf1b326beb70
Andy <andy@teamrubber.com>
2021-12-15 08:32:45 +0000

    Codechange: updated Polar Fox

--- src/polar_fox/constants.py
+++ src/polar_fox/constants.py
@@ -106,6 +106,8 @@ cargo_labels = [
     "STWR",
     "NH3_",
     "STSE",
+    "SEED",
+    "TATO",
     #
     "NULL",
 ]
@@ -271,8 +273,10 @@ default_cargos = {
     "coil": ["STST", "STAL", "STEL"],
     "coil_covered": ["STSH", "STEL"],
     "covered_ag": ["GRAI", "MAIZ", "BEAN", "NUTS", "FERT", "QLME"],
-    "covered_mineral": ["SALT", "PHOS", "POTA", "SAND", "KAOL", "NITR"],
+    "covered_chemical": ["RFPR", "POTA", "PHOS", "SALT", "SAND"],
+    "covered_mineral": ["QLME", "SALT", "PHOS", "POTA", "SAND", "KAOL", "NITR"],
     "covered_pellet": ["PLAS", "RUBR", "KAOL", "FERT", "SAND", "SASH", "BEAN"],
+    "covered_roller_roof": [ "KAOL", "QLME", "SALT"],
     "cryo_gases": ["O2__", "CHLO"],
     "dump": ["MNO2", "FECR", "NITR", "PHOS", "SAND", "GRVL"],
     "dump_high_sides": ["COKE", "PEAT", "COAL"],
