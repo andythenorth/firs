80f4d90b127db6e87f8e69855d4b3292c76a26bc
Andy <andy@teamrubber.com>
2022-09-18 11:16:13 +0100

    Codechange: use a more conventional 'import all, init all' approach for cargo registration

--- src/cargos/__init__.py
+++ src/cargos/__init__.py
@@ -1,429 +1,205 @@
 registered_cargos = []
-
 # keep these alphabetised for ease of maintaining
 from cargos import acid
-
-acid.cargo.register()
-
 from cargos import alcohol
-
-alcohol.cargo.register()
-
 from cargos import alloy_steel
-
-alloy_steel.cargo.register()
-
-"""
 from cargos import aluminia
-aluminia.cargo.register()
-"""
-
 from cargos import aluminium
-
-aluminium.cargo.register()
-
 from cargos import ammonia
-
-ammonia.cargo.register()
-
-"""
 from cargos import bauxite
-
-bauxite.cargo.register()
-"""
-
 from cargos import beans
-
-beans.cargo.register()
-
 from cargos import building_materials
-
-building_materials.cargo.register()
-
 from cargos import carbon_black
-
-carbon_black.cargo.register()
-
 from cargos import carbon_steel
-
-carbon_steel.cargo.register()
-
 from cargos import cassava
-
-cassava.cargo.register()
-
 from cargos import cast_iron
-
-cast_iron.cargo.register()
-
 from cargos import cement
-
-cement.cargo.register()
-
 from cargos import chemicals
-
-chemicals.cargo.register()
-
 from cargos import chlorine
-
-chlorine.cargo.register()
-
-""" IAHC
 from cargos import chromite_ore
-
-chromite_ore.cargo.register()
-"""
-
 from cargos import clay
-
-clay.cargo.register()
-
 from cargos import cleaning_agents
-
-cleaning_agents.cargo.register()
-
 from cargos import coal
-
-coal.cargo.register()
-
 from cargos import coal_tar
-
-coal_tar.cargo.register()
-
 from cargos import coffee
-
-coffee.cargo.register()
-
 from cargos import coke
-
-coke.cargo.register()
-
 from cargos import copper
-
-copper.cargo.register()
-
-""" IAHC
 from cargos import copper_concentrate
-
-copper_concentrate.cargo.register()
-"""
-
 from cargos import copper_ore
-
-copper_ore.cargo.register()
-
 from cargos import diamonds
-
-diamonds.cargo.register()
-
 from cargos import edible_oil
-
-edible_oil.cargo.register()
-
 from cargos import electrical_parts
-
-electrical_parts.cargo.register()
-
 from cargos import engineering_supplies
-
-engineering_supplies.cargo.register()
-
 from cargos import explosives
-
-explosives.cargo.register()
-
 from cargos import farm_supplies
-
-farm_supplies.cargo.register()
-
 from cargos import ferrochrome
-
-ferrochrome.cargo.register()
-
 from cargos import fertiliser
-
-fertiliser.cargo.register()
-
 from cargos import fish
-
-fish.cargo.register()
-
 from cargos import food
-
-food.cargo.register()
-
-""" BLTC
 from cargos import food_additives
-food_additives.cargo.register()
-"""
-
-""" IAHC
 from cargos import formic_acid
-
-formic_acid.cargo.register()
-"""
-
 from cargos import fruits
-
-fruits.cargo.register()
-
 from cargos import glass
-
-glass.cargo.register()
-
 from cargos import goods
-
-goods.cargo.register()
-
 from cargos import grain
-
-grain.cargo.register()
-
-""" BLTC
 from cargos import hydrochloric_acid
-hydrochloric_acid.cargo.register()
-"""
-
 from cargos import iron_ore
-
-iron_ore.cargo.register()
-
 from cargos import kaolin
-
-kaolin.cargo.register()
-
 from cargos import livestock
-
-livestock.cargo.register()
-
 from cargos import limestone
-
-limestone.cargo.register()
-
 from cargos import logs
-
-logs.cargo.register()
-
 from cargos import lye
-
-lye.cargo.register()
-
 from cargos import mail
-
-mail.cargo.register()
-
 from cargos import maize
-
-maize.cargo.register()
-
 from cargos import manganese
-
-manganese.cargo.register()
-
-"""
 from cargos import methanol
-methanol.cargo.register()
-"""
-
 from cargos import milk
-
-milk.cargo.register()
-
-""" BLTC
 from cargos import naphtha
-naphtha.cargo.register()
-"""
-
-"""
-# unused currently
 from cargos import nickel
-nickel.cargo.register()
-"""
-
 from cargos import nitrates
-
-nitrates.cargo.register()
-
 from cargos import nuts
-
-nuts.cargo.register()
-
 from cargos import oil
-
-oil.cargo.register()
-
 from cargos import oxygen
-
-oxygen.cargo.register()
-
-""" BLTC
 from cargos import packaging
-packaging.cargo.register()
-"""
-
 from cargos import paints_and_coatings
-
-paints_and_coatings.cargo.register()
-
 from cargos import paper
-
-paper.cargo.register()
-
 from cargos import passengers
-
-passengers.cargo.register()
-
 from cargos import peat
-
-peat.cargo.register()
-
 from cargos import petrol
-
-petrol.cargo.register()
-
 from cargos import phosphate
-
-phosphate.cargo.register()
-
-""" BLTC
 from cargos import phosphoric_acid
-phosphoric_acid.cargo.register()
-"""
-
 from cargos import pig_iron
-
-pig_iron.cargo.register()
-
 from cargos import pipe
-
-pipe.cargo.register()
-
 from cargos import plastics
-
-plastics.cargo.register()
-
 from cargos import potash
-
-potash.cargo.register()
-
 from cargos import pyrite_ore
-
-pyrite_ore.cargo.register()
-
 from cargos import quicklime
-
-quicklime.cargo.register()
-
-""" IAHC
 from cargos import raw_latex
-raw_latex.cargo.register()
-"""
-
-"""
 from cargos import recyclables
-recyclables.cargo.register()
-"""
-
 from cargos import rubber
-
-rubber.cargo.register()
-
 from cargos import sand
-
-sand.cargo.register()
-
 from cargos import salt
-
-salt.cargo.register()
-
 from cargos import scrap_metal
-
-scrap_metal.cargo.register()
-
 from cargos import slag
-
-slag.cargo.register()
-
 from cargos import soda_ash
-
-soda_ash.cargo.register()
-
 from cargos import stainless_steel
-
-stainless_steel.cargo.register()
-
 from cargos import steel
-
-steel.cargo.register()
-
 from cargos import steel_sections
-
-steel_sections.cargo.register()
-
 from cargos import steel_sheet
-
-steel_sheet.cargo.register()
-
 from cargos import steel_wire_rod
-
-steel_wire_rod.cargo.register()
-
 from cargos import stone
-
-stone.cargo.register()
-
 from cargos import sulphur
-
-sulphur.cargo.register()
-
-""" BLTC
 from cargos import sulphuric_acid
-sulphuric_acid.cargo.register()
-"""
-
 from cargos import timber
-
-timber.cargo.register()
-
-""" BLTC
 from cargos import tin
-tin.cargo.register()
-"""
-
-""" BLTC
 from cargos import tinplate
-tinplate.cargo.register()
-"""
-
 from cargos import tyres
-
-tyres.cargo.register()
-
 from cargos import vehicle_bodies
-
-vehicle_bodies.cargo.register()
-
 from cargos import vehicle_engines
-
-vehicle_engines.cargo.register()
-
 from cargos import vehicle_parts
-
-vehicle_parts.cargo.register()
-
 from cargos import vehicles
-
-vehicles.cargo.register()
-
 from cargos import wool
-
-wool.cargo.register()
-
-""" IAHC
 from cargos import yarn
-yarn.cargo.register()
-"""
-
 from cargos import zinc
 
+acid.cargo.register()
+alcohol.cargo.register()
+alloy_steel.cargo.register()
+# UNUSED aluminia.cargo.register()
+aluminium.cargo.register()
+ammonia.cargo.register()
+# UNUSED bauxite.cargo.register()
+beans.cargo.register()
+building_materials.cargo.register()
+carbon_black.cargo.register()
+carbon_steel.cargo.register()
+cassava.cargo.register()
+cast_iron.cargo.register()
+cement.cargo.register()
+chemicals.cargo.register()
+chlorine.cargo.register()
+# IAHC chromite_ore.cargo.register()
+clay.cargo.register()
+cleaning_agents.cargo.register()
+coal.cargo.register()
+coal_tar.cargo.register()
+coffee.cargo.register()
+coke.cargo.register()
+copper.cargo.register()
+# IAHC copper_concentrate.cargo.register()
+copper_ore.cargo.register()
+diamonds.cargo.register()
+edible_oil.cargo.register()
+electrical_parts.cargo.register()
+engineering_supplies.cargo.register()
+explosives.cargo.register()
+farm_supplies.cargo.register()
+ferrochrome.cargo.register()
+fertiliser.cargo.register()
+fish.cargo.register()
+food.cargo.register()
+# BLTC food_additives.cargo.register()
+# IAHC formic_acid.cargo.register()
+fruits.cargo.register()
+glass.cargo.register()
+goods.cargo.register()
+grain.cargo.register()
+# BLTC hydrochloric_acid.cargo.register()
+iron_ore.cargo.register()
+kaolin.cargo.register()
+livestock.cargo.register()
+limestone.cargo.register()
+logs.cargo.register()
+lye.cargo.register()
+mail.cargo.register()
+maize.cargo.register()
+manganese.cargo.register()
+# UNUSED methanol.cargo.register()
+milk.cargo.register()
+# BLTC naphtha.cargo.register()
+# UNUSED nickel.cargo.register()
+nitrates.cargo.register()
+nuts.cargo.register()
+oil.cargo.register()
+oxygen.cargo.register()
+# BLTC packaging.cargo.register()
+paints_and_coatings.cargo.register()
+paper.cargo.register()
+passengers.cargo.register()
+peat.cargo.register()
+petrol.cargo.register()
+phosphate.cargo.register()
+# BLTC phosphoric_acid.cargo.register()
+pig_iron.cargo.register()
+pipe.cargo.register()
+plastics.cargo.register()
+potash.cargo.register()
+pyrite_ore.cargo.register()
+quicklime.cargo.register()
+# IAHC raw_latex.cargo.register()
+# UNUSED recyclables.cargo.register()
+rubber.cargo.register()
+sand.cargo.register()
+salt.cargo.register()
+scrap_metal.cargo.register()
+slag.cargo.register()
+soda_ash.cargo.register()
+stainless_steel.cargo.register()
+steel.cargo.register()
+steel_sections.cargo.register()
+steel_sheet.cargo.register()
+steel_wire_rod.cargo.register()
+stone.cargo.register()
+sulphur.cargo.register()
+# BLTC sulphuric_acid.cargo.register()
+timber.cargo.register()
+# BLTC tin.cargo.register()
+# BLTC tinplate.cargo.register()
+tyres.cargo.register()
+vehicle_bodies.cargo.register()
+vehicle_engines.cargo.register()
+vehicle_parts.cargo.register()
+vehicles.cargo.register()
+wool.cargo.register()
+# IAHC yarn.cargo.register()
 zinc.cargo.register()
