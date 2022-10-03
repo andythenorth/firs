09cd75c00878caaf9fcd895bc45765b999f6137b
Andy <andy@teamrubber.com>
2022-10-03 21:06:13 +0100

    Codechange: aggregates, not stone

--- src/cargos/aggregates.py
+++ src/cargos/aggregates.py
@@ -1,10 +1,10 @@
 from cargo import Cargo
 
 cargo = Cargo(
-    id="stone",
-    type_name="string(STR_CARGO_NAME_STONE)",
-    unit_name="string(STR_CARGO_NAME_STONE)",
-    type_abbreviation="string(STR_CID_STONE)",
+    id="aggregates",
+    type_name="string(STR_CARGO_NAME_AGGREGATES)",
+    unit_name="string(STR_CARGO_NAME_AGGREGATES)",
+    type_abbreviation="string(STR_CID_AGGREGATES)",
     sprite="NEW_CARGO_SPRITE",
     weight="1.0",
     is_freight="1",
@@ -14,7 +14,7 @@ cargo = Cargo(
     town_growth_effect="TOWNGROWTH_NONE",
     town_growth_multiplier="1.0",
     units_of_cargo="TTD_STR_TONS",
-    items_of_cargo="string(STR_CARGO_UNIT_STONE)",
+    items_of_cargo="string(STR_CARGO_UNIT_AGGREGATES)",
     penalty_lowerbound="30",
     single_penalty_length="255",
     price_factor=91,
