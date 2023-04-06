3e94632c3addc15d15bbe0ea2dbbfadbe5a980d3
Andy <andy@teamrubber.com>
2023-04-06 22:20:00 +0100

    Change: set price factors for Steel Billets & Blooms, and Steel Slab

--- src/cargos/steel_slab.py
+++ src/cargos/steel_slab.py
@@ -18,7 +18,7 @@ cargo = Cargo(
     penalty_lowerbound="14",
     single_penalty_length="255",
     capacity_multiplier="1",
-    price_factor=127,
+    price_factor=128,
     icon_indices=(12, 4),
     sprites_complete=False,
 )
