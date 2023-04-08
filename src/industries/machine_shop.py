e99e6f7da4e6762d8f9d90924e3a109327e66221
Andy <andy@teamrubber.com>
2023-04-08 07:39:19 +0100

    Change: re-order cargos at Tracked Machine Factory

--- src/industries/machine_shop.py
+++ src/industries/machine_shop.py
@@ -3,11 +3,11 @@ from industry import IndustrySecondary, TileLocationChecks
 industry = IndustrySecondary(
     id="machine_shop",
     accept_cargos_with_input_ratios=[
-        ("VPTS", 3),
-        ("STSE", 1),
-        ("LFEQ", 1), # possibly redundant but eh, makes cargoflow look better
+        ("VPTS", 2),
         ("VENG", 1),
-        ("WELD", 2),
+        ("STSE", 3),
+        ("LFEQ", 1), # possibly redundant but eh, makes cargoflow look better
+        ("WELD", 1),
     ],
     prod_cargo_types_with_output_ratios=[
         # high output production is unwanted
