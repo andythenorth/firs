a164ac73c4bdcadf82aa9ab3e4d0c54f8d6d3368
Andy <andy@teamrubber.com>
2024-05-19 10:29:46 +0100

    Change: Appliance Factory done enough for v5

--- src/industries/appliance_factory.py
+++ src/industries/appliance_factory.py
@@ -22,7 +22,7 @@ industry = IndustrySecondary(
     nearby_station_name="string(STR_STATION_APPLIANCE_FACTORY)",
     fund_cost_multiplier="120",
     pollution_and_squalor_factor=1,
-    sprites_complete=False,
+    sprites_complete=True,
 )
 
 industry.enable_in_economy(
@@ -39,81 +39,171 @@ industry.add_tile(
     ),
 )
 
-spriteset_ground = industry.add_spriteset(
-    type="asphalt",
-)
+spriteset_ground = industry.add_spriteset(type="asphalt")
 spriteset_ground_overlay = industry.add_spriteset(type="empty")
-spriteset_shed_1 = industry.add_spriteset(
-    sprites=[(10, 10, 64, 80, -31, -50)],
+spriteset_1 = industry.add_spriteset(
+    sprites=[(10, 10, 64, 44, -31, -13)],
 )
-spriteset_shed_2 = industry.add_spriteset(
-    sprites=[(80, 10, 64, 80, -31, -50)],
+spriteset_2 = industry.add_spriteset(
+    sprites=[(80, 10, 64, 74, -31, -43)],
 )
-spriteset_tanks = industry.add_spriteset(
-    sprites=[(220, 10, 64, 80, -31, -50)],
+spriteset_3 = industry.add_spriteset(
+    sprites=[(150, 10, 64, 88, -31, -57)],
 )
-spriteset_office = industry.add_spriteset(
-    sprites=[(290, 10, 64, 80, -31, -50)],
+spriteset_4 = industry.add_spriteset(
+    sprites=[(220, 10, 64, 85, -31, -54)],
+)
+spriteset_5 = industry.add_spriteset(
+    sprites=[(290, 10, 64, 104, -31, -73)],
+)
+spriteset_6 = industry.add_spriteset(
+    sprites=[(360, 10, 64, 91, -31, -60)],
+)
+spriteset_7 = industry.add_spriteset(
+    sprites=[(430, 10, 64, 98, -31, -67)],
+)
+spriteset_8 = industry.add_spriteset(
+    sprites=[(500, 10, 64, 54, -31, -23)],
+)
+spriteset_9 = industry.add_spriteset(
+    sprites=[(570, 10, 64, 76, -31, -45)],
+)
+spriteset_10 = industry.add_spriteset(
+    sprites=[(640, 10, 64, 32, -31, -1)],
+)
+spriteset_11 = industry.add_spriteset(
+    sprites=[(710, 10, 64, 49, -31, -18)],
+)
+sprite_smoke_1 = industry.add_smoke_sprite(
+    smoke_type="white_smoke_big",
+    xoffset=17,
+    yoffset=9,
+    zoffset=99,
+)
+sprite_smoke_2 = industry.add_smoke_sprite(
+    smoke_type="white_smoke_big",
+    xoffset=20,
+    yoffset=9,
+    zoffset=100,
+    animation_frame_offset=1,
 )
 
 industry.add_spritelayout(
-    id="appliance_factory_spritelayout_empty",
+    id="appliance_factory_spritelayout_1",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_1],
+    fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_2",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_2],
+    fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_3",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_3],
+    fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_4",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_4],
+    fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_5",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_5],
+    smoke_sprites=[sprite_smoke_1, sprite_smoke_2],
+    fences=["nw", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_6",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_6],
+    fences=["nw", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_7",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_7],
+    fences=["nw", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_8",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_8],
+    fences=["nw", "ne", "se"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_9",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_9],
+    fences=["nw", "ne", "se"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_10",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_10],
+    fences=["nw", "ne", "se"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_11",
+    tile="appliance_factory_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_11],
+    fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="appliance_factory_spritelayout_12",
     tile="appliance_factory_tile_1",
     ground_sprite=spriteset_ground,
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[],
-    fences=[],
+    fences=["nw", "ne", "se"],
 )
-industry.add_spritelayout(
-    id="appliance_factory_spritelayout_shed_1",
-    tile="appliance_factory_tile_1",
-    ground_sprite=spriteset_ground,
-    ground_overlay=spriteset_ground_overlay,
-    building_sprites=[spriteset_shed_1],
-    fences=["nw", "ne"],
-    # add_to_object_num=5,
-)
-industry.add_spritelayout(
-    id="appliance_factory_spritelayout_shed_2",
-    tile="appliance_factory_tile_1",
-    ground_sprite=spriteset_ground,
-    ground_overlay=spriteset_ground_overlay,
-    building_sprites=[spriteset_shed_2],
-    fences=["ne"],
-    # add_to_object_num=5,
-)
-industry.add_spritelayout(
-    id="appliance_factory_spritelayout_office",
-    tile="appliance_factory_tile_1",
-    ground_sprite=spriteset_ground,
-    ground_overlay=spriteset_ground_overlay,
-    building_sprites=[spriteset_office],
-    fences=["nw", "ne", "se", "sw"],
-    # add_to_object_num=5,
-)
-industry.add_spritelayout(
-    id="appliance_factory_spritelayout_tanks",
-    tile="appliance_factory_tile_1",
-    ground_sprite=spriteset_ground,
-    ground_overlay=spriteset_ground_overlay,
-    building_sprites=[spriteset_tanks],
-    fences=["nw", "ne", "se", "sw"],
-    # add_to_object_num=5,
-)
+
 industry.add_industry_layout(
     id="appliance_factory_industry_layout_1",
     layout=[
-        (0, 0, "appliance_factory_spritelayout_shed_1"),
-        (0, 1, "appliance_factory_spritelayout_shed_1"),
-        (0, 2, "appliance_factory_spritelayout_shed_1"),
-        (0, 3, "appliance_factory_spritelayout_shed_2"),
-        (1, 0, "appliance_factory_spritelayout_shed_1"),
-        (1, 1, "appliance_factory_spritelayout_shed_1"),
-        (1, 2, "appliance_factory_spritelayout_empty"),
-        (1, 3, "appliance_factory_spritelayout_office"),
-        (2, 0, "appliance_factory_spritelayout_shed_1"),
-        (2, 1, "appliance_factory_spritelayout_shed_1"),
-        (2, 2, "appliance_factory_spritelayout_shed_2"),
-        (2, 3, "appliance_factory_spritelayout_tanks"),
+        (0, 0, "appliance_factory_spritelayout_12"),
+        (0, 1, "appliance_factory_spritelayout_12"),
+        (0, 2, "appliance_factory_spritelayout_11"),
+        (0, 3, "appliance_factory_spritelayout_12"),
+        (1, 0, "appliance_factory_spritelayout_12"),
+        (1, 1, "appliance_factory_spritelayout_8"),
+        (1, 2, "appliance_factory_spritelayout_9"),
+        (1, 3, "appliance_factory_spritelayout_10"),
+        (3, 0, "appliance_factory_spritelayout_12"),
+        (3, 1, "appliance_factory_spritelayout_5"),
+        (3, 2, "appliance_factory_spritelayout_6"),
+        (3, 3, "appliance_factory_spritelayout_7"),
+        (4, 0, "appliance_factory_spritelayout_1"),
+        (4, 1, "appliance_factory_spritelayout_2"),
+        (4, 2, "appliance_factory_spritelayout_3"),
+        (4, 3, "appliance_factory_spritelayout_4"),
     ],
 )
