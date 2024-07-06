929c9a42385f5c3d35b79c81a21ed7f0f363c614
Andy <andy@teamrubber.com>
2024-07-06 22:42:58 +0100

    Change: accept Paints & Coatings at Tracked Machine Factory instead of Pumps & Valves (which are sourced from same Precision Parts Plant as Vehicle Parts)

--- src/industries/tracked_machine_factory.py
+++ src/industries/tracked_machine_factory.py
@@ -6,7 +6,7 @@ industry = IndustrySecondary(
         ("VPTS", 2),
         ("VENG", 1),
         ("STSE", 3),
-        ("PUMP", 1),
+        ("COAT", 1),
         ("WELD", 1),
     ],
     prod_cargo_types_with_output_ratios=[
