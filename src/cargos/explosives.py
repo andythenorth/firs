433c4a03f32f02554dcbb91d1765a9200278d71b
Andy <andy@teamrubber.com>
2024-10-10 12:22:36 +0100

    Change: CC_ARMOURED does make pretty good sense for Explosives (tested with wide range of vehicle grfs, mostly appears in goods vans, armoured vans, mail vehicles)

--- src/cargos/explosives.py
+++ src/cargos/explosives.py
@@ -8,7 +8,8 @@ cargo = Cargo(
     sprite="NEW_CARGO_SPRITE",
     weight="0.25",
     is_freight="1",
-    cargo_classes="bitmask(CC_EXPRESS, CC_PIECE_GOODS)",
+    # armoured is a deliberate choice for explosives, tested with 10 of the major vehicle grfs in 2024, worked well
+    cargo_classes="bitmask(CC_EXPRESS, CC_PIECE_GOODS, CC_ARMOURED)",
     cargo_label="BOOM",
     units_of_cargo="TTD_STR_CRATES",
     items_of_cargo="string(STR_CARGO_UNIT_EXPLOSIVES)",
