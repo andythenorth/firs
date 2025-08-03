ccd890bc7e80fbc709a7673356186cacdb0e9b9f
Andy <andy@teamrubber.com>
2025-08-03 10:35:44 +0100

    Change: add gatehouse object for slag grinding plant

--- src/industries/slag_grinding_plant.py
+++ src/industries/slag_grinding_plant.py
@@ -95,6 +95,9 @@ spriteset_slag_dump_extra_1 = industry.add_spriteset(
 spriteset_slag_dump_extra_2 = industry.add_spriteset(
     sprites=[(710, 10, 64, 120, -31, -89)],
 )
+spriteset_gatehouse = industry.add_spriteset(
+    sprites=[(780, 10, 64, 120, -31, -89)],
+)
 spriteset_animated_dozer = industry.add_spriteset(
     sprites=[
         (10, 140, 64, 64, -31, -31),
@@ -145,7 +148,7 @@ industry.add_spritelayout(
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[],
     fences=["nw", "ne", "se", "sw"],
-    add_to_object_num=8,
+    add_to_object_num=9,
 )
 industry.add_spritelayout(
     id="slag_grinding_plant_spritelayout_large_silo",
@@ -240,7 +243,15 @@ industry.add_spritelayout(
     fences=["nw", "ne", "se", "sw"],
     add_to_object_num=2,
 )
-
+industry.add_spritelayout(
+    id="slag_grinding_plant_spritelayout_gatehouse",
+    tile="slag_grinding_plant_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_gatehouse],
+    fences=[], # no fences, entrance tile
+    add_to_object_num=8,
+)
 industry.add_multi_tile_object(
     add_to_object_num=4,
     view_layout=[
