31daa6de11f016c19b7d03998ac47bd41564a824
Andy <andy@teamrubber.com>
2026-09-11 18:52:08 +0100

    Change: add Mineral Additives to Cement Plant

--- src/industries/cement_plant.py
+++ src/industries/cement_plant.py
@@ -20,8 +20,9 @@ industry = IndustrySecondary(
 industry.enable_in_economy(
     "MILD_MILD_WEST",
     accept_cargos_with_input_ratios=[
-        ("COAL", 4),
+        ("COAL", 3),
         ("LIME", 4),
+        ("MADD", 1),
     ],
     prod_cargo_types_with_output_ratios=[
         ("CMNT", 7),
